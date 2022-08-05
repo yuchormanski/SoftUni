2a046d624632acae0f2814c0ee56563fc51ce900
Nikolay Yuchormanski <693307+yuchormanski@users.noreply.github.com>
2022-08-05 08:20:15 +0300

    Create primeNumber.js

---
+++ JS INFO/primeNumber.js
@@ -0,0 +1,11 @@
+function primeNumberChecker(n) {
+    let isPrime = true;
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    isPrime === true ? console.log(true) : console.log(false);
+}
+//primeNumberChecker(35)
