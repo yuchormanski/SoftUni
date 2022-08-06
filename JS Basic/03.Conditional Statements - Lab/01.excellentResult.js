ab200e4c73e2f8e920c8e4ddaa39149320aa8d32
Nikolay Yuchormanski <693307+yuchormanski@users.noreply.github.com>
2022-08-06 10:31:21 +0300

    Add files via upload

---
+++ JS Basic/03.Conditional Statements - Lab/01.excellentResult.js
@@ -0,0 +1,4 @@
+function excellentResult(grade) {
+    grade >= 5.50 ? console.log(`Excellent!`) : null;
+}
+excellentResult((["5.49"]))
---
+++ JS Basic/03.Conditional Statements - Lab/03.evenOrOdd.js
@@ -0,0 +1,5 @@
+function evenOrOdd(input) {
+let n = Number(input[0]);
+n % 2 === 0? console.log(`even`): console.log(`odd`);;
+}
+evenOrOdd(["3"])
