8a32524cf1fe28a16e05356ec8c33d1f2553bb67
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-09-08 10:27:56 +0300

    fixed chessBoard

--- JS Fundamentals - 05.22/09.Exercise Data Types and Variables/16.chessBoard.js
+++ JS Fundamentals - 05.22/09.Exercise Data Types and Variables/16.chessBoard.js
@@ -1,19 +1,25 @@
 function chessBoard(n) {
-    let counter = 0;
+    let counter = 1;
     console.log(`<div class="chessboard">`);
     for (let i = 1; i <= n; i++) {
         console.log(`  <div>`);
-        for (let j = 1; j <= n; j++) {
-            counter++;
-            if (counter % 2 !== 0) { console.log(`    <span class="black"></span>`); }
-            else { console.log(`    <span class="white"></span>`); }
+        for (let j = 1; j <= n; j++, counter++) { //increase counter after first cycle
+            if (counter % 2 !== 0) {
+                console.log(`    <span class="black"></span>`);
+            }
+            else {
+                console.log(`    <span class="white"></span>`);
+            }
         }
-        counter++
         console.log(`  </div>`);
+        // if board is even number fields -> change first color
+        if (n % 2 === 0) {
+            counter++;
+        }
     }
     console.log(`</div>`);
 }
-chessBoard(6)
+chessBoard(3)
 /* 
 6.	Chess Board
 Write a function to print a chessboard of size n X n. See the example for more information.
