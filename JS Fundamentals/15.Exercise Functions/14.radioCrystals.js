9201b1266f7388779a639179b57f113e00b777d1
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-09-17 08:20:33 +0300

    done

--- JS Fundamentals/15.Exercise Functions/14.radioCrystals.js
+++ JS Fundamentals/15.Exercise Functions/14.radioCrystals.js
@@ -1,93 +1,83 @@
 function radioCrystals(arr) {
-    let desired = arr[0];
-    let crystal = arr[1]
-
-    console.log(`Processing chunk ${crystal} microns`);
-    // Cut
-    if (crystal / 4 >= desired) {
-        let cut = 0;
-        while (crystal > desired * 4) {
-            crystal /= 4;
-            cut++;
+    let i = 0;
+    let [desired, crystal] = [arr.shift(), arr[i]];
+    let [currentCrystal, crystalCount] = [0, arr.length];
+    while (currentCrystal < crystalCount) {
+        console.log(`Processing chunk ${crystal} microns`);
+        // Cut
+        if (crystal / 4 >= desired) {
+            let cut = 0;
+            while (crystal >= desired * 4) {
+                crystal /= 4;
+                cut++;
+            }
+            console.log(`Cut x${cut}`);
+            console.log(`Transporting and washing`);
+            if (!Number.isInteger(crystal)) {
+                crystal = Math.trunc(crystal)
+            }
         }
-        console.log(`Cut x${cut}`);
-        console.log(`Transporting and washing`);
-        if (!Number.isInteger(crystal)) {
-            crystal = Math.trunc(crystal)
-
+        // End Cut
+        // Lap
+        if (crystal * 0.8 >= desired) {
+            let lap = 0;
+            while (crystal >= desired * 1.2) {
+                crystal *= 0.8;
+                lap++;
+            }
+            console.log(`Lap x${lap}`);
+            console.log(`Transporting and washing`);
+            if (!Number.isInteger(crystal)) {
+                crystal = Math.trunc(crystal)
+            }
         }
+        //End Lap
+        // Grind
+        if (crystal - 20 >= desired) {
+            let grind = 0;
+            while (crystal >= desired + 20) {
+                crystal -= 20;
+                grind++;
+            }
+            console.log(`Grind x${grind}`);
+            console.log(`Transporting and washing`);
+            if (!Number.isInteger(crystal)) {
+                crystal = Math.trunc(crystal)
+            }
+        }
+        //End Grind
+        // Etch
+        if (crystal - 2 >= desired) {
+            let etch = 0;
+            while (crystal > desired) {
+                crystal -= 2;
+                etch++;
+            }
+            console.log(`Etch x${etch}`);
+            console.log(`Transporting and washing`);
+            if (!Number.isInteger(crystal)) {
+                crystal = Math.trunc(crystal)
+            }
+        }
+        //End Etch
+        //X-ray
+        if (crystal < desired) {
+            crystal += 1;
+            console.log(`X-ray x1`);
+            if (!Number.isInteger(crystal)) {
+                crystal = Math.trunc(crystal)
+            }
+        }
+        //End X-ray
+        // Finish
+        if (crystal === desired) {
+            console.log(`Finished crystal ${desired} microns`);
+        }
+        currentCrystal++;
+        crystal = arr[++i];
     }
-    // End Cut
-
-
-    // Lap
-    if (crystal * 0.8 >= desired) {
-        let lap = 0;
-        while (crystal > desired) {
-            crystal *= 0.8;
-            lap++;
-        }
-        console.log(`Lap x${lap}`);
-        console.log(`Transporting and washing`);
-        if (!Number.isInteger(crystal)) {
-            crystal = Math.trunc(crystal)
-
-        }
-    }
-    //End Lap
-
-    // Grind
-    if (crystal - 20 >= desired) {
-        let grind = 0;
-        while (crystal > desired) {
-            crystal -= 20;
-            grind++;
-        }
-        console.log(`Grind x${grind}`);
-        console.log(`Transporting and washing`);
-        if (!Number.isInteger(crystal)) {
-            crystal = Math.trunc(crystal)
-
-        }
-    }
-    //End Grind
-
-    // Etch
-    if (crystal - 2 >= desired) {
-        let etch = 0;
-        while (crystal > desired) {
-            crystal -= 2;
-            etch++;
-        }
-        console.log(`Grind x${etch}`);
-        console.log(`Transporting and washing`);
-        if (!Number.isInteger(crystal)) {
-            crystal = Math.trunc(crystal)
-
-        }
-    }
-    //End Etch
-
-    //X-ray
-    if (crystal < desired) {
-        crystal += 1;
-        console.log(`X-ray x1`);
-        if (!Number.isInteger(crystal)) {
-            crystal = Math.trunc(crystal)
-        }
-    }
-    //End X-ray
-
-    // End
-
-    if (crystal === desired) {
-        console.log(`Finished crystal ${desired} microns`);
-    }
-
-
-
 }
-radioCrystals([1000, 4000, 8100])
+radioCrystals([1375, 50000])
 
 /* 14.	Radio Crystals 
 You need to write a JS program that monitors the current thickness of the crystal and 
