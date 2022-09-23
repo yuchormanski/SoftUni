15a8ef01c4d11805444ec6f5f6881f83a9fd0563
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-09-23 10:50:07 +0300

    20/100

--- JS Fundamentals/12.Exercise Arrays/10.ladybugs.js
+++ JS Fundamentals/12.Exercise Arrays/10.ladybugs.js
@@ -1,27 +1,77 @@
-function ladybugs(moves) {
-    let area = moves.shift();
-    let bugs = moves.shift().split(' ');
-    let field = [];
-    for(let f = 0; f < area; f++){
-        if(bugs[f] >= 0){
-            field.push(1);
+function ladybug(mainArray) {
+    let fieldLength = mainArray.shift();
+    let moves = mainArray.shift().split(' ').join('');
+    let [field, , startIndex, direction, targetIndex] = [[], 0, '', 0];
+    let bug;
+
+    makeField(); //   field creation
+    fillField(); //   field occupation 
+
+    for (let i = 0; i < mainArray.length; i++) {
+        //taking each separate move and split it to strings
+        let currentMove = mainArray[i].split(' ');
+        startIndex = Number(currentMove[0]);
+        direction = currentMove[1];
+        if (direction === 'right') {
+            targetIndex = startIndex + Number(currentMove[2]);
         } else {
-            field.push(0);
+            targetIndex = startIndex - Number(currentMove[2]);
+        }
+        seekingPlace()
+    }
+    console.log(field.join(' '));
+
+    //make field
+    function makeField() {
+        for (let i = 0; i < fieldLength; i++) {
+            field.splice(i, 1, 0)
         }
     }
-    
-
-    for (i = 0; i < moves.length; i++) {
-        let currentMove = moves[i].split(' ');
-        let r = currentMove.includes('right');
-        let l = currentMove.includes('left');
-        if(r) {
-            if(currentMove[i] + currentMove[2])
-            bug[i] = bug[i] + currentMove[2];
+    //fill field
+    function fillField() {
+        for (let j = 0; j < moves.length; j++) {
+            bug = moves[j]
+            field.splice(bug, 1, 1)
         }
+    }
+    // change targetIndex until is empty
+    function seekingPlace() {
+
+        if (field[startIndex] === 0 || startIndex < 0) { // IF given index is 0 or startIndex is out of field
+            return;
+        } else if (field[targetIndex] < 0 || field[targetIndex] >= field.length) { // If target index is outside the field
+            return;
+        } else if (field[targetIndex] === 0) {
+            field.splice(startIndex, 1, 0); //free startIndex place
+            field.splice(targetIndex, 1, 1); //land on endIndex place
+        } else if (field[targetIndex] === 1) {
+            while (field[targetIndex] !== 0) {
+                if (direction === 'right') {
+                    field[++targetIndex];
+                } else if (direction === 'left') {
+                    field[--targetIndex];
+                } else if (direction !== 'right' || direction !== 'left') { // IF direction is different than RIGHT or LEFT
+                    return;
+                }
+                if (targetIndex < 0 || targetIndex >= field.length) {
+                    return outOfField()
+                }
+            }
+            field.splice(startIndex, 1, 0); //free startIndex place
+            field.splice(targetIndex, 1, 1); //land on endIndex place
+        } else if (targetIndex < 0 || targetIndex >= field.length) {
+            outOfField()
+        }
+    }
+    // IF targetIndex is out of field
+    function outOfField() {
+        return field.splice(startIndex, 1, 0); //free startIndex place
     }
 }
-ladybugs([3, '0 1', '0 right 1', '2 right 1'])
+ladybug([3, '0 1', '-1 right 1', '0 right 1', '2 right 1', '1 right 5'])
+//ladybug([3, '0 1', '0 right 1', '2 right 1'])
+//ladybug([3, '0 1 2', '0 right 1', '1 right 1', '2 right 1'])
+//ladybug([5, '3', '2 right 1', '3 left 2', '1 left -2'])
 
 /* 10.	* Ladybugs
 You are given a field size and the indexes of ladybugs inside the field. A ladybug changes its position either to its 
