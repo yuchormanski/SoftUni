d1e7c211757cac50fe3d34ef9d49fd95bfbd0998
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-09-27 12:28:06 +0300

    new

---
+++ JS Fundamentals Archive/04.Methods. Debugging and Troubleshooting Code - Lab/02.signOfIntegerNumber.js
@@ -0,0 +1,15 @@
+/* 2.	Sign of Integer Number
+Create a method that prints the sign of an integer number n.
+Examples
+Input	                    Output
+2	                        The number 2 is positive.
+-5	                        The number -5 is negative.
+0	                        The number 0 is zero. */
+
+function signOfIntegerNumber(number) {
+    number < 0 ? console.log(`The number ${number} is negative.`) :
+        number > 0 ? console.log(`The number ${number} is positive.`) :
+            number === 0 ? console.log(`The number ${number} is zero.`) : null;
+
+}
+signOfIntegerNumber(-5)
