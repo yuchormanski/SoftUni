5a3a15c98787e5c62ce48211219c8fe4ea1bb39f
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-05 08:35:02 +0300

    unfinished

--- JS Fundamentals/19.Exam Preparation/03.heartDelivery.js
+++ JS Fundamentals/19.Exam Preparation/03.heartDelivery.js
@@ -9,8 +9,10 @@ The jump commands will be in this format: "Jump {length}".
 Every time he jumps from one house to another, the needed hearts for the visited house are decreased by 2: 
 •	If the needed hearts for a certain house become equal to 0, print on the console 
 "Place {house_index} has Valentine's day." 
+
 •	If Cupid jumps to a house where the needed hearts are already 0, print on the console 
 "Place {house_index} already had Valentine's day."
+
 •	Keep in mind that Cupid can have a larger jump length than the size of the neighborhood, 
 and if he does jump outside of it, he should start from the first house again (index 0)
 For example, we are given this neighborhood: 6@6@6. Cupid is at the start and jumps with a 
@@ -21,6 +23,7 @@ Input
 •	On the first line, you will receive a string with even integers separated by "@" – the 
 neighborhood and the number of hearts for each house.
 •	On the next lines, until "Love!" is received, you will be getting jump commands in this format: "Jump {length}".
+
 Output
 In the end, print Cupid's last position and whether his mission was successful or not:
 •	"Cupid's last position was {last_position_index}."
@@ -28,6 +31,7 @@ In the end, print Cupid's last position and whether his mission was successful o
 o	"Mission was successful."
 •	If not, print the count of all houses that didn't celebrate Valentine's Day:
 o	"Cupid has failed {houseCount} places."
+
 Constraints
 •	The neighborhood's size will be in the range [1…20]
 •	Each house will need an even number of hearts in the range [2 … 10]
@@ -44,8 +48,48 @@ Input	                 Output	                                Comments
                                                                 The following command is "Love!" so we print Cupid's 
                                                                 last position and the outcome of his mission.
 
-  */                                                               
-function heartDelivery() {
+  */
+function heartDelivery(valentinesArray) {
+  let arrKeeper = valentinesArray.slice(0,1);
+  arrKeeper  = arrKeeper.shift().split('@').map(Number);
+  let neighborhood = valentinesArray.shift().split('@').map(Number);
+  
+  let commands = valentinesArray.slice(0);
+  let index;
+  let newIndex;
+  let notVisitedHouse = 0;
+  for (let i = 0; i <= valentinesArray.length; i++) {
+    let currentHouse = neighborhood[i];
+    let jump = commands.shift().split(' ');
+    let step = Number(jump[1]);
+    let isVisited = true;
+    
+    //TODO: when reaching Love
+    if (jump[0] === "Love!") {
+      console.log(`Cupid's last position was ${newIndex}.`);
+      break;
+    }
+    index = neighborhood.indexOf(currentHouse);
+    if (currentHouse === 0) {
+      console.log(`Place ${index} already had Valentine's day."`);
+    } else {
+      newIndex = index + step;
+      neighborhood[newIndex] -= 2;
 
+      // IF when reaching 0
+      if (neighborhood[newIndex] === 0) {
+        console.log(`Place ${newIndex} has Valentine's day." `);
+      }
+    } 
+  }
+  for(let el = 0; el < arrKeeper.length; el++){
+    if(arrKeeper[el] === neighborhood[el]) {
+      isVisited = false;
+      notVisitedHouse++;
+    }
+  }
+  if(!isVisited){
+      console.log(`Cupid has failed ${notVisitedHouse} places."`);
+    }
 }
 heartDelivery(["10@10@10@2", "Jump 1", "Jump 2", "Love!"])
