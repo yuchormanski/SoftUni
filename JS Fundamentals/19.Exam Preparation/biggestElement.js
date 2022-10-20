fc1499683adbc29d330d6c9c20f87a87d786ec26
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-20 08:00:40 +0300

    unfinished

--- JS Fundamentals/19.Exam Preparation/biggestElement.js
+++ JS Fundamentals/19.Exam Preparation/biggestElement.js
@@ -38,3 +38,6 @@ function biggestElement(matrix) {
 biggestElement([3 5 17 12 91 5,
                 -1 7 4 33 6 22,
                 1 8 99 3 10 43])
+
+
+                judge input - [ [ 3, 5, 7, 12 ], [ -1, 4, 33, 2 ], [ 8, 3, 0, 4 ] ]
