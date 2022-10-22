c26f3994d22224f72b0fd4188bfa7dd634a4a05e
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-22 17:56:41 +0300

    edited

--- JS Fundamentals/19.Exam Preparation/02.theLift.js
+++ JS Fundamentals/19.Exam Preparation/02.theLift.js
@@ -1,31 +1,29 @@
 
-function theLift(touristOnLift) {
-    let tourist = Number(touristOnLift.shift());
-    let lift = touristOnLift.join(' ').split(" ").map(Number);
-    let maxPeople = 4;
-    let totalAvailable = lift.length * maxPeople;
-    for (let cabin = 0; cabin < lift.length; cabin++) {
-        
-        let currentInLift = lift[cabin];
-        let toEnter = maxPeople - currentInLift;
-        if (toEnter > tourist) { toEnter = tourist }
-        lift[cabin] += toEnter;
-        tourist -= toEnter;
-        totalAvailable -= toEnter;
-        if (tourist <= 0) {
-            tourist = 0;
-            break;
-        }
-    }
-    if (tourist === 0) {
-        if (totalAvailable > 0) {
-            console.log('The lift has empty spots!');
-        }
-    } else {
-        console.log(`There isn't enough space! ${tourist} people in a queue!`);
-    }
-    console.log(`${lift.join(' ')}`);
+function theLift(liftArray) {
+    let passengers = Number(liftArray.slice(0, 1));
+    let wagons = liftArray.slice(-1).join('').split(' ').map(Number);
 
+    for (let i = 0; i < wagons.length; i++) {
+        let freeSpace = 4 - wagons[i];
+        if (freeSpace > 0) {
+            if (passengers >= freeSpace) {
+                passengers -= freeSpace;
+                freeSpace = 0;
+                wagons[i] = 4;
+            } else {
+                wagons[i] += passengers;
+                passengers -= freeSpace;
+                break;
+            }
+        }
+    }
+    if (passengers > 0) {
+        console.log(`There isn't enough space! ${passengers} people in a queue!`);
+    } else if (passengers < 0) {
+        console.log('The lift has empty spots!');
+    }
+    console.log(wagons.join(' '));
 }
-theLift(["15", "0 0 0 0 0"])
-theLift(["20", "0 2 0"])
+// theLift(["15", "0 0 0 0 0"])
+// theLift(["20", "0 2 0"])
+theLift(["12", "0 0 0"])
