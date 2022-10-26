0372b473c717a5e2002387ae248a9f3ebbc5094f
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-26 08:41:23 +0300

    up

--- JS Fundamentals/18.Exercise Arrays Advanced/10.build_aWall.js
+++ JS Fundamentals/18.Exercise Arrays Advanced/10.build_aWall.js
@@ -47,9 +47,30 @@ Input	Output
 Input	Output
 [17, 22, 17, 19, 17]	975, 975, 975, 975, 975, 975, 975, 975, 780, 780, 780, 585, 585 
 21489000 pesos  */
-function build_aWall(){
+function build_aWall(wall) {
+    let sum = 0;
+    let concrete = 195;
+    let finishedTotal = wall.length * 30;
+    let total = wall.reduce((el, x) => el + x);
+    let buff = [];
+    let totalSum = 0;
+
+    while (total < finishedTotal) {
+        for (let i = 0; i < wall.length; i++) {
+            if (wall[i] < 30) {
+                wall[i]++;
+                sum += concrete;
+            }
+        }
+        buff.push(sum);
+        totalSum += sum;
+        sum = 0;
+        total = wall.reduce((el, x) => el + x);
+    }
+    console.log(buff.join(', '));
+    console.log(`${totalSum * 1900} pesos`);
 
 }
-build_aWall([21, 25, 28])
+build_aWall([17, 22, 17, 19, 17])
 
 
