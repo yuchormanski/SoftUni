b6dfba988bdb9c39c7bacc600cf84af457e6331e
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-29 09:40:25 +0300

    up

--- JS Fundamentals/22.Exercise_Objects and Classes/inClass.js
+++ JS Fundamentals/22.Exercise_Objects and Classes/inClass.js
@@ -31,3 +31,16 @@ function test2(data) {
 // test2(['Sofia | 42.696552 | 23.32601',
 //     'Beijing | 39.913818 | 116.363625']
 // )
+
+function test3(store, delivery) {
+    const stock = {};
+
+
+    for (let i = 0; i < store.length; i += 2) {
+        const current = store[i];
+        stock[current] = store[i + 1];
+    }
+    console.log(store);
+}
+test3(['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
+    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'])
