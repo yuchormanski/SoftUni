2b7b7f5fb51b5e80b27c6a1ec424213155b21611
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-05 09:25:48 +0200

    task added

--- JS Fundamentals/25.Exercise_Associative Arrays/08.travelTime.js
+++ JS Fundamentals/25.Exercise_Associative Arrays/08.travelTime.js
@@ -1,3 +1,15 @@
+/* *Travel Time 
+Write a function that collects and orders information about travel destinations. 
+As input, you will receive an array of strings. 
+Each string will consist of the following information with the format: 
+"Country name > Town name > Travel cost" 
+The Country name will be a string, the Town name will be a unique string, Travel cost will be a number. 
+If you receive the same Town name twice, you should keep the cheapest offer. 
+Have in mind that one Country may have several Towns to visit. 
+After you finish the organizational part, you need to let Steven know which 
+destination point to visit first. The order will be as follows:  First sort Country names alphabetically 
+and then sort by lowest Travel cost.  */
+
 function travelTime(data) {
     //object
     let countries = {};
@@ -12,15 +24,17 @@ function travelTime(data) {
             if (!countries[country]) {
                 countries[country] = {};
             }
-
+            // create child object
             if (!countries[country][town]) {
                 countries[country][town] = price;
             }
+            //comparing child object value and taking lower
             if (countries[country][town] > price) {
                 countries[country][town] = price;
             }
         }
     }
+    //sort parent by a-b
     let sortedCountries = Object.keys(countries).sort((a, b) => a.localeCompare(b));
 
     for (let country of sortedCountries) {
