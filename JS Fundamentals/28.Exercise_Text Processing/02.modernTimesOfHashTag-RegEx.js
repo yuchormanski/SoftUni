8f59faac177f822f62e69675c1723feb21b9e5cb
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-15 23:37:44 +0200

    update

---
+++ JS Fundamentals/28.Exercise_Text Processing/02.modernTimesOfHashTag-RegEx.js
@@ -0,0 +1,6 @@
+function modernTimesOfHashTagRegEx(inputString) {
+    inputString.match(/\#[A-Za-z]+/g).forEach((element) => console.log(element.slice(1)));
+}
+modernTimesOfHashTagRegEx(
+    '#Nowadays everyone uses # to #1jhk tag a #special word in #socialMedia'
+);
