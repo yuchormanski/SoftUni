b5193484c92a61fef42f7995af9c2c8ee0b64350
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-15 23:38:37 +0200

    done with RegEx

--- JS Fundamentals/28.Exercise_Text Processing/02.modernTimesOfHashTag-RegEx.js
+++ JS Fundamentals/28.Exercise_Text Processing/02.modernTimesOfHashTag-RegEx.js
@@ -1,6 +1,4 @@
 function modernTimesOfHashTagRegEx(inputString) {
     inputString.match(/\#[A-Za-z]+/g).forEach((element) => console.log(element.slice(1)));
 }
-modernTimesOfHashTagRegEx(
-    '#Nowadays everyone uses # to #1jhk tag a #special word in #socialMedia'
-);
+modernTimesOfHashTagRegEx('#Nowadays everyone uses # to #1jhk tag a #special word in #socialMedia');
