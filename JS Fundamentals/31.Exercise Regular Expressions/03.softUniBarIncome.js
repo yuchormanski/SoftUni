123a5e6b881710f1e839508016ccfa696e606dce
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-18 23:40:10 +0200

    fail

--- JS Fundamentals/31.Exercise Regular Expressions/03.softUniBarIncome.js
+++ JS Fundamentals/31.Exercise Regular Expressions/03.softUniBarIncome.js
@@ -5,10 +5,7 @@ function softUniBarIncome(line) {
     let command = line.shift();
 
     while (command !== 'end of shift') {
-
         let currentData = pattern.exec(command);
-        console.log(currentData);
-
         if (currentData) {
             let currentCustomer = currentData.groups['name'];
             let currentProduct = currentData.groups['product'];
@@ -22,10 +19,4 @@ function softUniBarIncome(line) {
     }
     console.log(`Total income: ${totalIncome.toFixed(2)}`);
 }
-softUniBarIncome(['%George%<Croissant>|2|10.3$', '%George%<Croissant>|2|10.3$', '%Peter%<Gum>|1|1.3$', '%Maria%<Cola>|1|2.4$', 'end of shift']);
-// softUniBarIncome(['%InvalidName%<Croissant>|2|10.3$',
-// '%Peter%<Gum>1.3$',
-// '%Maria%<Cola>|1|2.4',
-// '%Valid%<Valid>valid|10|valid20$',
-// 'end of shift']
-// )
+softUniBarIncome(['%George%<Croissant>|2|10.3$', '%Peter%<Gum>|1|1.3$', '%Maria%<Cola>|1|2.4$', 'end of shift']);
