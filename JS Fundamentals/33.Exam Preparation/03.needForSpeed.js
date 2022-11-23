382c1e5de9e1a95052ee08f361917b7d4955af3c
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-24 00:26:29 +0200

    done!

--- JS Fundamentals/33.Exam Preparation/03.needForSpeed.js
+++ JS Fundamentals/33.Exam Preparation/03.needForSpeed.js
@@ -105,30 +105,30 @@ function needForSpeed(data) {
     }
 
 }
-// needForSpeed([
-//     '3',
-//     'Audi A6|38000|62',
-//     'Mercedes CLS|11000|35',
-//     'Volkswagen Passat CC|45678|5',
-//     'Drive : Audi A6 : 543 : 47',
-//     'Drive : Mercedes CLS : 94 : 11',
-//     'Drive : Volkswagen Passat CC : 69 : 8',
-//     'Refuel : Audi A6 : 50',
-//     'Revert : Mercedes CLS : 500',
-//     'Revert : Audi A6 : 30000',
-//     'Stop'
-// ]);
-
 needForSpeed([
-    '4',
-    'Lamborghini Veneno|11111|74',
-    'Bugatti Veyron|12345|67',
-    'Koenigsegg CCXR|67890|12',
-    'Aston Martin Valkryie|99900|50',
-    'Drive : Koenigsegg CCXR : 382 : 82',
-    'Drive : Aston Martin Valkryie : 99 : 23',
-    'Drive : Aston Martin Valkryie : 2 : 1',
-    'Refuel : Lamborghini Veneno : 40',
-    'Revert : Bugatti Veyron : 2000',
+    '3',
+    'Audi A6|38000|62',
+    'Mercedes CLS|11000|35',
+    'Volkswagen Passat CC|45678|5',
+    'Drive : Audi A6 : 543 : 47',
+    'Drive : Mercedes CLS : 94 : 11',
+    'Drive : Volkswagen Passat CC : 69 : 8',
+    'Refuel : Audi A6 : 50',
+    'Revert : Mercedes CLS : 500',
+    'Revert : Audi A6 : 30000',
     'Stop'
 ]);
+
+// needForSpeed([
+//     '4',
+//     'Lamborghini Veneno|11111|74',
+//     'Bugatti Veyron|12345|67',
+//     'Koenigsegg CCXR|67890|12',
+//     'Aston Martin Valkryie|99900|50',
+//     'Drive : Koenigsegg CCXR : 382 : 82',
+//     'Drive : Aston Martin Valkryie : 99 : 23',
+//     'Drive : Aston Martin Valkryie : 2 : 1',
+//     'Refuel : Lamborghini Veneno : 40',
+//     'Revert : Bugatti Veyron : 2000',
+//     'Stop'
+// ]);
