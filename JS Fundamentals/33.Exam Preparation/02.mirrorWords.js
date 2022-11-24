9ca2a7025d32f01d1fa8a9326fa3b08ac66af6b0
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-11-24 18:51:32 +0200

    done!

--- JS Fundamentals/33.Exam Preparation/02.mirrorWords.js
+++ JS Fundamentals/33.Exam Preparation/02.mirrorWords.js
@@ -1,16 +1,56 @@
+/* On the first line of the input, you will be given a text string. To win the competition, you have to find 
+all hidden word pairs, read them, and mark the ones that are mirror images of each other.
+First of all, you have to extract the hidden word pairs. Hidden word pairs are:
+•	Surrounded by "@" or "#" (only one of the two) in the following pattern #wordOne##wordTwo# or @wordOne@@wordTwo@
+•	At least 3 characters long each (without the surrounding symbols)
+•	Made up of letters only
+If the second word, spelled backward, is the same as the first word and vice versa (casing matters!), 
+they are a match, and you have to store them somewhere. Examples of mirror words: 
+#Part##traP# @leveL@@Level@ #sAw##wAs#
+•	If you don`t find any valid pairs, print: "No word pairs found!"
+•	If you find valid pairs print their count: "{valid pairs count} word pairs found!"
+•	If there are no mirror words, print: "No mirror words!"
+•	If there are mirror words print:
+"The mirror words are:
+{wordOne} <=> {wordtwo}, {wordOne} <=> {wordtwo}, … {wordOne} <=> {wordtwo}"
+Input / Constraints
+•	You will recive a string.
+Output
+•	Print the proper output messages in the proper cases as described in the problem description.
+•	If there are pairs of mirror words, print them in the end, each pair separated by ", ".
+•	Each pair of mirror word must be printed with " <=> " between the words. */
+
+
 function mirrorWords(line) {
-    let pattern = /(#|@)(?<first>[A-Za-z]{3,})\1\1(?<second>[A-Za-z]{3,})\1/gi;
-
+    let pattern = /([#|@])([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1/gi
     let input = line.join('');
-    let collection = [];
     let found = input.match(pattern);
-    console.log(found);
-
-
-
-
-
+    let matchCount;
+    if (found === null) {
+        console.log('No word pairs found!\nNo mirror words!');
+        return;
+    } else {
+       matchCount = found.length;
+    }
+    let pairs = [];
+    for (let el of found) {
+        let first = el.slice(1, el.length / 2 - 1);
+        let second = el.slice(el.length / 2 + 1, -1);
+        let reversed = second.split('').reverse().join('');
+        if (first === reversed) {
+            pairs.push(`${first} <=> ${second}`);
+        }
+    }
+    console.log(`${matchCount} word pairs found!`);
+    if (pairs.length > 0) {
+        console.log('The mirror words are:');
+        console.log(pairs.join(', '));
+    } else {
+        console.log('No mirror words!');
+    }
 }
-mirrorWords([
-    '@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'
-]);
+mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r']);
+
+// mirrorWords(['#po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@']);
+
+// mirrorWords(['#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#'])
---
+++ JS Fundamentals/34.Exam Preparation/01.theImitationGame.js
@@ -0,0 +1,53 @@
+/* On the first line of the input you will receive the encrypted message. 
+After that, until the "Decode" command is given, you will be receiving strings 
+with instructions for different operations that need to be performed upon the 
+concealed message to interpret it and reveal its true content. There are several 
+types of instructions, split by '|'
+•	Move {number of letters}
+o	Moves the first n letters to the back of the string. 
+•	Insert {index} {value}
+o	Inserts the given value before the given index in the string.
+•	ChangeAll {substring} {replacement} 
+o	Changes all occurrences of the given substring with the replacement text.
+Input / Constraints
+•	On the first line, you will receive a string with message.
+•	On the next lines, you will be receiving commands, split by '|' .
+Output
+•	After the "Decode" command is received, print this message:
+"The decrypted message is: {message}" */
+
+
+function theImitationGame(line) {
+    let message = line.shift();
+    let currentCommand = line.shift();
+
+    while (currentCommand) {
+        let [command, arg1, arg2] = currentCommand.split('|');
+        if (command === 'ChangeAll') {
+            message = message.split(arg1).join(arg2);
+        } else if (command === 'Insert') {
+            arg1 = Number(arg1);
+            message = message.slice(0, arg1) + arg2 + message.slice(arg1);
+        } else if (command === 'Move') {
+            arg1 = Number(arg1);
+            let cut = message.slice(0, arg1);
+            message = message.slice(arg1) + cut;
+        }
+        currentCommand = line.shift();
+    }
+    console.log(`The decrypted message is: ${message}`);
+}
+// theImitationGame([
+//     'zzHe',
+//     'ChangeAll|z|l',
+//     'Insert|2|o',
+//     'Move|3',
+//     'Decode'
+// ]);
+theImitationGame([
+    'owyouh',
+    'Move|2',
+    'Move|3',
+    'Insert|3|are',
+    'Insert|9|?',
+    'Decode'])
