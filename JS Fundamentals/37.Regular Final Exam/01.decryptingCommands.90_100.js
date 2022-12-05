9891543c9e19b12abb36deeaee68f7fc6348cef7
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-05 08:23:51 +0200

    90/100

--- JS Fundamentals/37.Regular Final Exam/01.decryptingCommands.90_100.js
+++ JS Fundamentals/37.Regular Final Exam/01.decryptingCommands.90_100.js
@@ -1,3 +1,39 @@
+/* 01.Decrypting-Commands-условие
+Problem 1 - Decrypting Commands
+
+On the first line, you are going to receive a string. You will receive "decrypting" commands on the following lines
+until you get the "Finish" command. There are five possible commands:
+
+"Replace {currentChar} {newChar}"
+。Replace all occurrences of the current char with the new char, then print the resulting message. 
+
+"Cut {startIndex} {endIndex}"
+。Remove the substring from the start index until the end index (both inclusive), then print the resulting message.
+If any of the indexes is not valid, print: "Invalid indices!".
+
+"Make (Upper/Lower}"
+。 Replace all letters with upper/lower case and print the resulting message.
+
+"Check {string}"
+。If the message contains the given string, print: "Message contains {string}".
+Otherwise, print: "Message doesn't contain {string}".
+
+"Sum {startIndex} {endIndex}"
+。Get the substring from the start index to the end index (both inclusive) and print the sum of the ASCII values of the substring.
+If any of the given indices are invalid, print: "Invalid indices!". An index is valid when it is non- negative 
+and less than the size of the collection.
+
+Note: At any time, the message will contain at least one character.
+
+Input
+• On the first line, you are going to receive the string.
+Output
+On the following lines, until the "Finish" command is received, you will be receiving commands.
+Print the output of every command in the format described above.
+Constraints
+The indexes will be integers in the range [-50...150]. */
+
+
 function decryptingCommands(input) {
     let data = input.slice();
     let message = data.shift();
