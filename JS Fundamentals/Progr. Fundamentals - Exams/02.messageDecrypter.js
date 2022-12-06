a14520f13fc4085cd67a8d335c3f46b70998a8f5
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-06 21:59:25 +0200

    done!

--- JS Fundamentals/Progr. Fundamentals - Exams/02.messageDecrypter.js
+++ JS Fundamentals/Progr. Fundamentals - Exams/02.messageDecrypter.js
@@ -6,10 +6,10 @@ A message is valid when:
 It starts with a tag, which is surrounded by either "$" or "%" (but not both at the same time). 
 The tag itself has to be minimum 3 characters long, start with an uppercase letter, followed only by lowercase letters
 There is a colon and a single white space after the tag
-There are 3 groups consisting of numbers between
-and "]", followed by a pipe ("|")
+There are 3 groups consisting of numbers between "[" and "]", followed by a pipe ("|")
+
 Example for a valid message:
-"$Requests: [73]|[115]|[32]|"
+"$Request$: [73]|[115]|[32]|"
 You must check if the message is valid, and if it is - decrypts it. If it isn't - print the following message:
 "Valid message not found!"
 Decrypting a message means taking all numbers and turn them into ASCII symbols. After successful decrypt, 
@@ -22,9 +22,55 @@ On the next n lines - input that you have to check if it has a valid message.
 • Print all results from each input, each on a new line. */
 
 
+function messageDecrypter(input) {
+    let data = input.slice();
+    let lineCount = data.shift();
+    let regex = /^(\$|%)(?<tag>[A-Z][a-z]{2,})\1: \[(?<first>[\d]+)\]\|\[(?<second>[\d]+)\]\|\[(?<third>[\d]+)\]\|$/;
 
+    while (lineCount > 0) {
+        let line = data.shift();
+        let valid = regex.exec(line);
 
-function messageDecrypter(){
+        if (valid) {
+            let tag = valid.groups['tag'];
+            let first = valid.groups['first'];
+            let second = valid.groups['second'];
+            let third = valid.groups['third'];
+            first = String.fromCharCode(first);
+            second = String.fromCharCode(second);
+            third = String.fromCharCode(third);
 
+            console.log(`${tag}: ${first + second + third}`);
+        } else {
+            console.log('Valid message not found!');
+        }
+        lineCount--;
+    }
 }
-messageDecrypter()
+// messageDecrypter([
+//     "4",
+//     "$Request$: [73]|[115]|[105]|",
+//     "%Taggy$: [73]|[73]|[73]|",
+//     "%Taggy%: [118]|[97]|[108]|",
+//     "$Request$: [73]|[115]|[105]|[32]|[75]|"
+// ]);
+
+/* Output
+Request: Isi
+Valid message not found!
+Taggy: val
+Valid message not found! */
+
+
+messageDecrypter([
+    "3",
+    "This shouldnt be valid%Taggy%: [118]|[97]|[108]|",
+    "$tAGged$: [97][97][97]|",
+    "$Request$: [73]|[115]|[105]|true"
+]);
+
+/* Output
+Valid message not found!
+Valid message not found!
+Valid message not found! */
+
