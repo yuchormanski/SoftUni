ea9926d4370e470196395aaadb2f29161b52b812
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-07 23:14:14 +0200

    up

---
+++ JS Fundamentals/37.b.Regular Final Exam - Other Tasks/02.encryptingPassword.js
@@ -0,0 +1,17 @@
+function encryptingPassword() {
+
+}
+encryptingPassword([
+    "3",
+    "##>00|no|NO|!!!?<###",
+    "##>123|yes|YES|!!!<##",
+    "$$<111|noo|NOPE|<<>$$"
+]);
+
+encryptingPassword(["5",
+    "aa>111|mqu|BAU|mqu<aa",
+    "()>111!aaa!AAA!^&*<()",
+    "o>088|abc|AAA***<o",
+    "asd>asd|asd|ASD|asd<asd",
+    "*>088|zzzz|ZzZ|123<*"
+]);
