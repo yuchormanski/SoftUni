5b59a0c230e7b52985f5d0455ecfd7498c63ea63
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-11 15:08:35 +0200

    up

---
+++ JS Advanced/04.Exercise - Syntax, Functions and Statements/07.cookingByNumbers.js
@@ -0,0 +1,22 @@
+function cookingByNumbers(num, a, b, c, d, e) {
+    num = Number(num);
+    let data = [a, b, c, d, e];
+
+    data.forEach(command => {
+        switch (command) {
+            case 'chop':
+                num /= 2; break;
+            case 'dice':
+                num = Math.sqrt(num); break;
+            case 'spice':
+                num += 1; break;
+            case 'bake':
+                num *= 3; break;
+            case 'fillet':
+                num -= num * 0.2; break;
+        }
+        console.log(num);
+    });
+}
+// cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
