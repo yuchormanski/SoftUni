feecb1d35c267cc639a98f6dd8bc2be57eb2845a
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-14 08:36:26 +0200

    inProgress

--- 03.JS Advanced/06.Exercise - Arrays and Nested Arrays/10.ticTacToe.js
+++ 03.JS Advanced/06.Exercise - Arrays and Nested Arrays/10.ticTacToe.js
@@ -4,11 +4,29 @@ function ticTacToe(moves) {
     for (let i = 0; i < length; i++) {
         let temp = [];
         for (let j = 0; j < length; j++) {
-            temp.push('false');
+            temp.push(false);
         }
         board.push(temp);
     }
-    console.table(board)
+    // console.table(board) 
+    for (const move of moves) {
+        let [playerO, playerX] = move.split(' ');
+        playerO = Number(playerO);
+        playerX = Number(playerX);
+        currentMove();
+        function currentMove(){
+            for(let turn of board[playerO]){ /// не става с фороф - направи фор цикъл
+                if(turn === false){
+                    turn = 'O';
+                    return;
+                }else{
+                    console.log("This place is already taken. Please choose another!");
+                }
+            }
+        }
+
+    }
+    console.table(board) // view board - debug
 
 }
 ticTacToe([
