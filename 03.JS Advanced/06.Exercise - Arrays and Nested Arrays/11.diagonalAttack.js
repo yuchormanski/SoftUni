19182542966011a7c96d15cab74753a0b5b09208
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-15 08:36:57 +0200

    done!

--- 03.JS Advanced/06.Exercise - Arrays and Nested Arrays/11.diagonalAttack.js
+++ 03.JS Advanced/06.Exercise - Arrays and Nested Arrays/11.diagonalAttack.js
@@ -2,15 +2,18 @@ function diagonalAttack(input) {
     let data = input.slice();
     const main = [];
 
+    //creating matrix
     data.forEach(line => {
         let row = line.split(' ').map(Number);
         main.push(row);
     });
-    
+
+    // matrix/row length will be used often so create variable
     let length = main.length - 1;
     let leftSum = 0;
     let rightSum = 0;
 
+    // summing bot diagonals values
     for (let j = 0; j <= length; j++) {
         let leftIndex = main[0 + j][j];
         let rightIndex = main[0 + j][length - j];
@@ -18,19 +21,22 @@ function diagonalAttack(input) {
         rightSum += rightIndex;
     }
 
+    // sum comparison cases
     leftSum === rightSum ? changeOthers(leftSum) : printResult();
 
+    //change non diagonal values to diagonal sum value
     function changeOthers(toThis) {
         for (let j = 0; j <= length; j++) {
-            for(let el = 0; el <= length; el++){
-                if(el !== j && el !== length - j){
+            for (let el = 0; el <= length; el++) {
+                if (el !== j && el !== length - j) {
                     main[j][el] = toThis;
                 }
             }
         }
         return printResult();
     }
-    
+
+    // print matrix
     function printResult() {
         main.forEach(row => console.log(row.join(' ')));
     }
