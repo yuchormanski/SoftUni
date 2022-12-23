f309fc85153b8303b58b18fc48d1939442f49e78
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-23 06:54:30 +0200

    up

--- 02.JS Fundamentals/Additinal Tasks/juniorsTest.js
+++ 02.JS Fundamentals/Additinal Tasks/juniorsTest.js
@@ -3,3 +3,44 @@
 - Накрая трябва да се принтира 3-тото най-голямо число и индекс позицията му в дадения масив.
 Подсказка: внимавайте за повтарящи числа.
 >> Примерен вход: [91, 2, 33, 51, 54, 39, 34, 61, 34, 91]; */
+
+function test(data) {
+    let first = Number.MIN_SAFE_INTEGER;
+    let second = Number.MIN_SAFE_INTEGER;
+    let third = Number.MIN_SAFE_INTEGER;
+    let fIndex = 0;
+    let sIndex = 0;
+    let tIndex = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        let num = data[i];
+        if(num > first && num > second && num > third){
+            let tIndex = 0;
+            let temp = first;
+            first = num;
+            fIndex = i;
+            [second, temp] = [temp, second];
+            [third,temp] = [temp, third];
+            if(data.includes(second)){
+                sIndex = data.indexOf(second);
+            }
+            if(data.includes(third)){
+                tIndex = data.indexOf(third);
+            }
+        } else if(num < first && num > second && num > third){
+            let temp = second;
+            second = num;
+            [sIndex,tIndex] = [tIndex, sIndex];
+            sIndex = i;
+            [third,temp] = [temp, third];
+            tIndex = tIndex;
+        } else if(num < first && num < second && num > third){
+            third = num;
+            tIndex = i;
+        }
+    }
+    // console.log(`${third}\n${tIndex}`);
+    console.log(`${first} ${fIndex}\n${second} ${sIndex}\n${third} ${tIndex}`);
+
+}
+test([91, 2, 33, 51, 54, 39, 34, 61, 34, 91]);
