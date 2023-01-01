7caea425c2d8c015bdc8095c6a726deabeb4ee4b
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 15:49:42 +0200

    up

--- 03.JS Advanced/09.DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ 03.JS Advanced/09.DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
+    let row = document.querySelectorAll('tr');
+    for (let i = 0; i < row.length; i++) {
+        i % 2 !== 0 ? row[i].style.background = "Teal" : null;
+    }
+
+    // let i = 0;
+    // for (let el of row) {
+    //     i++ % 2 !== 0 ? el.style.background = "Teal" : null;
+    // }
+
 }
