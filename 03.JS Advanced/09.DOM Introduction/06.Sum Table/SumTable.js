4f5a51add4b33c0db473474c9281eabf8696aaef
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 16:10:56 +0200

    up

--- 03.JS Advanced/09.DOM Introduction/06.Sum Table/SumTable.js
+++ 03.JS Advanced/09.DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
-}
+    let totalArray = [];
+    let sums = document.querySelectorAll('td');
+    for (let el of sums) {
+        !isNaN(el.textContent) ? totalArray.push(Number(el.textContent)) : null;
+    }
+    let total = totalArray.reduce((x, y) => x + y);
+    let result = document.querySelector('#sum');
+    result.textContent = total;
+};
