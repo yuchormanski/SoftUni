1019b8f302d5b956560525cd62b9af0a5095828c
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 18:42:06 +0200

    up

--- 03.JS Advanced/09.DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ 03.JS Advanced/09.DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
-
+    let text = document.getElementById(content).textContent;
+    let regex = /(?:\()(?<word>[A-Za-z\s]+)(?:\))/gm;
+    let all = [];
+    let res = regex.exec(text);
+    while(res !== null){
+        all.push(res.groups['word']);
+        res = regex.exec(text);
+    }
+    let result = all.join('; ')
+    return result;
 }
