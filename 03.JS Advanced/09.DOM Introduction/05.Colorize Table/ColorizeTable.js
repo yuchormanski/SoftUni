b6d51b0d7791c66047c6538f2d2dc551e0766bf1
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 21:30:36 +0200

    update

--- 03.JS Advanced/09.DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ 03.JS Advanced/09.DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,12 +1,15 @@
 function colorize() {
-    let row = document.querySelectorAll('tr');
-    for (let i = 0; i < row.length; i++) {
-        i % 2 !== 0 ? row[i].style.background = "Teal" : null;
-    }
+    // let row = document.querySelectorAll('tr');
+    // for (let i = 0; i < row.length; i++) {
+    //     i % 2 !== 0 ? row[i].style.background = "Teal" : null;
+    // }
 
     // let i = 0;
     // for (let el of row) {
     //     i++ % 2 !== 0 ? el.style.background = "Teal" : null;
     // }
+
+    let row = document.querySelectorAll('tr:nth-of-type(2n) td');
+    row.forEach(x => x.style.backgroundColor = 'teal');
 
 }
--- 03.JS Advanced/09.DOM Introduction/06.Sum Table/SumTable.js
+++ 03.JS Advanced/09.DOM Introduction/06.Sum Table/SumTable.js
@@ -4,6 +4,7 @@ function sumTable() {
     for (let el of sums) {
         !isNaN(el.textContent) ? totalArray.push(Number(el.textContent)) : null;
     }
+    let last = totalArray.pop();
     let total = totalArray.reduce((x, y) => x + y);
     let result = document.querySelector('#sum');
     result.textContent = total;
