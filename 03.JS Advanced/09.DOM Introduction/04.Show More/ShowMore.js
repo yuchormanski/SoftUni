55cd59966572cbb964a3f110f567debdc2370de4
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 15:17:37 +0200

    up

--- 03.JS Advanced/09.DOM Introduction/04.Show More/ShowMore.js
+++ 03.JS Advanced/09.DOM Introduction/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
     // TODO
+    let link = document.getElementById('more');
+    let hidden = document.getElementById('text');
+    link.style.display = 'none';
+    hidden.style.display = 'inline';
 }
