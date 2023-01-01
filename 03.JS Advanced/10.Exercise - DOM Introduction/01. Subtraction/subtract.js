d5a29aefb333973e1a1c24e5dba3d727e06ec768
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 19:23:19 +0200

    up

--- 03.JS Advanced/10.Exercise - DOM Introduction/01. Subtraction/subtract.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    let first = Number(document.querySelector('#firstNumber').value);
+    let second = Number(document.querySelector('#secondNumber').value);
+    let subtract = first - second;
+    let result = document.getElementById('result');
+    result.textContent = subtract;
+    console.log(subtract);
 }
