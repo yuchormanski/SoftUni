9d4d69a773ca0c5ce1de8a6a0651fb40984ec70b
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-01 14:53:26 +0200

    up

--- 03.JS Advanced/09.DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ 03.JS Advanced/09.DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,8 @@
 function calc() {
     // TODO: sum = num1 + num2
+    let firstNum = document.querySelector('#num1');
+    let secondNum = document.querySelector('#num2');
+    let sumFields = Number(firstNum.value) + Number(secondNum.value);
+    let result = document.querySelector('#sum');
+    result.value = sumFields;
 }
