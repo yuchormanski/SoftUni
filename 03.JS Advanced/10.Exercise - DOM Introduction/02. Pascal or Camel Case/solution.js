51ea61f0ca238c15a4c8ce2cc4b3f8ec55de0419
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-02 11:05:04 +0200

    up

--- 03.JS Advanced/10.Exercise - DOM Introduction/02. Pascal or Camel Case/solution.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  let inputText = document.querySelector('#text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let toArray = inputText.toLowerCase().split(' ');
+  let result = [];
+
+  if (convention === 'Camel Case') {
+    result.push(toArray.shift());
+    toArray.forEach(element => {
+      let firstLetter = element.slice(0, 1).toUpperCase();
+      let restLetters = element.slice(1);
+      let word = firstLetter + restLetters;
+      result.push(word);
+    });
+    result = result.join('');
+  } else if (convention === 'Pascal Case') {
+    toArray.forEach(element => {
+      let firstLetter = element.slice(0, 1).toUpperCase();
+      let restLetters = element.slice(1);
+      let word = firstLetter + restLetters;
+      result.push(word);
+    });
+    result = result.join('');
+  } else {
+    result = 'Error!'
+  }
+
+  let resultElement = document.getElementById('result');
+  resultElement.textContent = result;
 }
