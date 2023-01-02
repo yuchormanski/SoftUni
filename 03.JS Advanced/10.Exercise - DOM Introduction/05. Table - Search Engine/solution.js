f86ee8a03ed425055e2758bf8ccac79bae2994c2
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-02 20:24:56 +0200

    up

--- 03.JS Advanced/10.Exercise - DOM Introduction/05. Table - Search Engine/solution.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let search = document.querySelector('#searchField').value;
+      let tableRow = document.querySelectorAll('table.container tbody tr');
 
+      for (let element of tableRow) {
+         element.classList.remove('select');
+         if (element.textContent.includes(search)) {
+            element.className = 'select';
+         }
+      }
+      search = '';
    }
 }
