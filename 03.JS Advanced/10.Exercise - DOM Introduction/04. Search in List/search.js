c3035cd0f6ae5737bc812ee602ddea4de09f60fe
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-02 16:17:43 +0200

    up

--- 03.JS Advanced/10.Exercise - DOM Introduction/04. Search in List/search.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let listItems = document.querySelectorAll('#towns li');
+   let searchElement = document.querySelector('#searchText').value;
+   let resultDiv = document.querySelector('#result');
+
+
+   for(let el of listItems){
+      el.style.textDecoration = 'none';
+      el.style.fontWeight = 'normal';
+   };
+
+   let matchCounter = 0;
+
+   for (let el of listItems) {
+      if (el.textContent.includes(searchElement)) {
+         el.style.textDecoration = 'underline';
+         el.style.fontWeight = 'bold';
+         matchCounter++;
+      }
+   }
+   resultDiv.textContent = `${matchCounter} matches found`;
 }
