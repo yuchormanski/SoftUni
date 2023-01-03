79777600c5fcd02ac637804763299cdfc0923a91
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-03 22:00:40 +0200

    80

--- 03.JS Advanced/10.Exercise - DOM Introduction/06. Format the Text/solution.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+
+  let input = document.getElementById('input').value.trim();
+  let result = document.getElementById('output');
+  let sentences = input.split('. ');
+  let buffer = ``;
+  let temp = [];
+
+  while (sentences.length !== 0) {
+
+    for (let i = 0; i < 3 && sentences.length > 0; i++) {
+
+      let current = sentences.shift();
+      if (!current.endsWith('.')) {
+        temp.push(`${current}.`);
+      } else {
+        temp.push(`${current}`);
+      }
+    }
+    buffer+=(`<p>${temp.join(' ')}</p>`);
+    temp = [];
+  };
+  result.innerHTML = buffer;
 }
