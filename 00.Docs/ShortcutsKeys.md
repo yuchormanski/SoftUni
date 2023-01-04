8e8978e590fa5f0441d1d45ac738d4c3c64ec706
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-04 20:22:13 +0200

    up

---
+++ 00.Docs/ShortcutsKeys.md
@@ -0,0 +1,28 @@
+|   Key combination  |          Description        |
+| ------------------ | --------------------------- |
+|      Ctrl + A      |  Select All                 | 
+|      Ctrl + B      |  Bold                       |
+|      Ctrl + C      |  Copy                       |
+|      Ctrl + D      |  Default font               |
+|      Ctrl + E      |  Center                     |
+|      Ctrl + F      |  Find                       |
+|      Ctrl + G      |  Go to                      |
+|      Ctrl + H      |  Replace                    |
+|      Ctrl + I      |  Italic                     |
+|      Ctrl + J      |  Justify                    |
+|      Ctrl + K      |  Hyperlink                  |
+|      Ctrl + L      |  Align text to Left         |
+|      Ctrl + M      |  Hanging Indent             |
+|      Ctrl + N      |  New document               |
+|      Ctrl + O      |  Open                       |
+|      Ctrl + P      |  Print                      |
+|      Ctrl + Q      |  Add space after paragraph  |
+|      Ctrl + R      |  Align text to right        |
+|      Ctrl + S      |  Safe as                    |
+|      Ctrl + T      |  Left indent                |
+|      Ctrl + U      |  Underline                  |
+|      Ctrl + V      |  Paste                      |
+|      Ctrl + W      |  Close                      |
+|      Ctrl + X      |  Cut                        |
+|      Ctrl + Y      |  Redo / Repeat              |
+|      Ctrl + Z      |  Undo                       |
--- 03.JS Advanced/10.Exercise - DOM Introduction/07. Hell's Kitchen/app.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/07. Hell's Kitchen/app.js
@@ -1,8 +1,54 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+
+   function onClick() {
+      let inputElement = JSON.parse(document.querySelector('#inputs textarea').value);
+      let avgSalary = 0;
+      let totalSalary = 0;
+      let currentAvgSalary = 0;
+      let bestName = '';
+      let bestSalary = 0;
+      const output = {};
+
+
+      for (let line of inputElement) {
+         let restInfo = line.split(' - ');
+         let restName = restInfo[0];
+         let workersData = restInfo[1].split(', ');
+
+         for (let worker of workersData) {
+            let [name, salary] = worker.split(' ');
+            if (!output[restName]) {
+               output[restName] = {};
+            }
+            if (output[restName]) {
+               output[restName][name] = Number(salary);
+            }
+         }
+
+      }
+      let entries = Object.entries(output);
+
+      for (let entry of entries) {
+
+         let key = entry[0];
+         let values = Object.values(entry[1]);
+
+         for (let salary of values) {
+            totalSalary += salary;
+         }
+         avgSalary = totalSalary / values.length;
+         if (avgSalary > currentAvgSalary) {
+            currentAvgSalary = avgSalary;
+            bestName = key;
+            totalSalary = 0;
+         }
+      }
+      let result = Object.entries(output[bestName]).sort((a,b) => b[1] - a[1]);
+      let print = '';
+
+      result.forEach()
+console.log(result);
    }
 }
