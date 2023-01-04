6604250b9c9a11c5e4450c8faf0e501f620ded13
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-04 07:13:09 +0200

    up

--- 03.JS Advanced/10.Exercise - DOM Introduction/06. Format the Text/solution.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/06. Format the Text/solution.js
@@ -2,23 +2,12 @@ function solve() {
 
   let input = document.getElementById('input').value.trim();
   let result = document.getElementById('output');
-  let sentences = input.split('. ');
-  let buffer = ``;
-  let temp = [];
+  let sentences = input.split('.').filter(x => x !== '');
 
   while (sentences.length !== 0) {
-
-    for (let i = 0; i < 3 && sentences.length > 0; i++) {
-
-      let current = sentences.shift();
-      if (!current.endsWith('.')) {
-        temp.push(`${current}.`);
-      } else {
-        temp.push(`${current}`);
-      }
-    }
-    buffer+=(`<p>${temp.join(' ')}</p>`);
-    temp = [];
-  };
-  result.innerHTML = buffer;
+    let current = sentences.splice(0, 3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = current;
+    result.appendChild(p);
+  }
 }
