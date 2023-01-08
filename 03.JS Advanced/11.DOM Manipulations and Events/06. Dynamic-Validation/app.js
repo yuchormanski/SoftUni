fd2db1f63c20b6f1df02d06469b198fdbdc9a977
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-09 00:17:45 +0200

    up

--- 03.JS Advanced/11.DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ 03.JS Advanced/11.DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let inputEmail = document.getElementById('email');
+
+    let regex = /\b([a-z]+@[a-z]+\.[a-z]+)\b/g;
+    
+    inputEmail.addEventListener('change', (e) => {
+        if(e.target.value.match(regex)){
+            e.target.classList.remove('error');
+        } else {
+            e.target.classList.add('error');
+        }
+    });
 }
