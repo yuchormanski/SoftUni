228d3b92aa24ad8766f59890a31b84b254b96d56
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-08 21:15:02 +0200

    up

--- 03.JS Advanced/11.DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ 03.JS Advanced/11.DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let result = document.getElementById('result');
+
+    let gradientHandler = (e) => {
+        //to find event methods console.log(e)
+        let percent = Math.floor((e.offsetX / e.target.clientWidth) * 100);
+        console.log(e);
+        result.textContent = `${percent}%`;
+    }
+    gradientElement.addEventListener('mousemove', gradientHandler);
 }
+
