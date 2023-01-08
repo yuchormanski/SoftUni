47f2b2fab53fa7884e0dbc8a585d3781e691d614
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-08 23:44:51 +0200

    currentTarget / target

--- 03.JS Advanced/11.DOM Manipulations and Events/05. Highlight-Active/app.js
+++ 03.JS Advanced/11.DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,23 @@
 function focused() {
-    console.log('TODO:...');
+    let boxSelection = document.querySelectorAll('div input');
+
+    // let func = function(e){
+    //     e.currentTarget.parentNode.className = 'focused';
+    // }
+
+    // for (let el of boxSelection) {
+    //     el.addEventListener('mouseover', func);
+    //     el.addEventListener('mouseout', (e) => {
+    //         e.currentTarget.parentNode.classList.remove('focused');
+    //     });
+    // }
+
+    for (let el of boxSelection) {
+        el.addEventListener('focus', (e) =>{
+            e.target.parentNode.className = 'focused';
+        })
+        el.addEventListener('blur', (e) =>{
+            e.target.parentNode.classList.remove('focused');
+        })
+    }
 }
