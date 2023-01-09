17ac6b5156fbad3bd8651e92d61599419567be6a
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-09 22:04:54 +0200

    done

--- 03.JS Advanced/10.Exercise - DOM Introduction/09. Number Convertor/solution.js
+++ 03.JS Advanced/10.Exercise - DOM Introduction/09. Number Convertor/solution.js
@@ -1,33 +1,43 @@
 function solve() {
-    document.getElementById('input').addEventListener('change', onChange);
 
-    function onChange() {
-        let options = '<option selected value="binary">Binary</option><option selected value="hexadecimal">Hexadecimal</option>';
-        let inputField = document.getElementById('input').value;
-        inputField = Number(inputField);
-        let convertTo = document.getElementById('selectMenuTo');
-        let result = document.getElementById('result');
-        convertTo.innerHTML = options;
+    let inputElement = document.getElementById('input');
+    let selectElement = document.getElementById('selectMenuTo');
+    let result = document.getElementById('result');
 
-        document.querySelector('button').addEventListener('click', onClick);
+    let buttonElement = document.querySelector('#container button');
 
-        function onClick() {
-            if (convertTo.value === 'binary') {
-                result.value = inputField.toString(2);
-            } else if (convertTo.value === 'hexadecimal') {
-                let base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-                let hex = [];
-                let reminder = 0;
-                while (inputField !== 0) {
-                    let current = inputField / 16;
-                    inputField = parseInt(inputField / 16)
-                    if (current - parseInt(current) > 0) {
-                        reminder = (current - parseInt(current)) * 16;
-                    }
-                    hex.unshift(base[reminder]);
+    let binaryElement = document.createElement('option');
+    binaryElement.setAttribute('selected', '');
+    binaryElement.setAttribute('value', '');
+    binaryElement.value = 'binary';
+    binaryElement.textContent = 'Binary';
+
+    let hexElement = document.createElement('option');
+    hexElement.setAttribute('selected', '');
+    hexElement.setAttribute('value', '');
+    hexElement.value = 'hexadecimal';
+    hexElement.textContent = 'Hexadecimal';
+
+    selectElement.appendChild(binaryElement);
+    selectElement.appendChild(hexElement);
+
+    buttonElement.addEventListener('click', () => {
+        if (selectElement.value === 'binary') {
+            result.value = Number(inputElement.value).toString(2);
+        } else if (selectElement.value === 'hexadecimal') {
+            inputField = Number(inputElement.value);
+            let base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+            let hex = [];
+            let reminder = 0;
+            while (inputField !== 0) {
+                let current = inputField / 16;
+                inputField = parseInt(inputField / 16)
+                if (current - parseInt(current) > 0) {
+                    reminder = (current - parseInt(current)) * 16;
                 }
-                result.value = hex.join('').toUpperCase();
+                hex.unshift(base[reminder]);
             }
+            result.value = hex.join('').toUpperCase();
         }
-    }
+    });
 }
