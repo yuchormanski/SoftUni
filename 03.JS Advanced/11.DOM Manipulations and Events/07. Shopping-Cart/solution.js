15d4edcdd30f72b8caec37231a78c3b885461634
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-09 08:31:20 +0200

    75%

--- 03.JS Advanced/11.DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 03.JS Advanced/11.DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+
+   let productEl = document.querySelectorAll('.product');
+   let outputElement = document.querySelector('textarea');
+   let checkoutEl = document.querySelector('.checkout');
+   let buttonsEl = document.querySelectorAll('.add-product');
+   let totalPrice = 0;
+   const shoppingBag = [];
+
+   let print = () => {
+      outputElement.textContent += `You bought ${shoppingBag.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      buttonsEl.forEach(e => e.setAttribute('disabled', ''));
+      checkoutEl.setAttribute('disabled', '');
+   };
+
+   for (let el of productEl) {
+      el.addEventListener('click', (e) => {
+         let item = e.currentTarget.children[1].children[0].textContent;
+         let price = e.currentTarget.children[3].textContent;
+         price = Number(price);
+         outputElement.textContent += `Added ${item} for ${price.toFixed(2)} to the cart.\n`;
+         totalPrice += price;
+         shoppingBag.push(item);
+      });
+   }
+
+   checkoutEl.addEventListener('click', print)
 }
