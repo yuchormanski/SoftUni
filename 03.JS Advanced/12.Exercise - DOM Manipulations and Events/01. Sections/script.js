ee48948ac460e677d4eb8098b9bbca153465cbd5
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-10 07:49:46 +0200

    addComments

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/01. Sections/script.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/01. Sections/script.js
@@ -1,17 +1,17 @@
 function create(words) {
-   let contentElements = document.getElementById('content');
+   let contentElements = document.getElementById('content'); // get divs holder
 
-   for (let el of words) {
-      let divEl = document.createElement('div');
-      let paragraphEl = document.createElement('p');
-      paragraphEl.style.display = 'none';
-      paragraphEl.textContent = el;
-      divEl.appendChild(paragraphEl);
-      contentElements.appendChild(divEl);
+   for (let el of words) { // for every string of input ...
+      let divEl = document.createElement('div');  //create div to store paragraph
+      let paragraphEl = document.createElement('p');  //create paragraph
+      paragraphEl.style.display = 'none';  // add paragraph style
+      paragraphEl.textContent = el;  // set text to paragraph to current string from input
+      divEl.appendChild(paragraphEl); //append paragraph to created div
+      contentElements.appendChild(divEl); // append created div to divs holder
    }
 
    let reveal = function (e) {
-      // e.target.querySelector('p').style.display = 'block';
+      // e.target.querySelector('p').style.display = 'block';  //change style on target paragraph
       // OR
       e.target.firstChild.style.display ='block';
    }
