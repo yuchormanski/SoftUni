60e5cdbb4bcb1cb0a1af29aa3383e041265ccef4
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-10 07:43:50 +0200

    up

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/01. Sections/script.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElements = document.getElementById('content');
+
+   for (let el of words) {
+      let divEl = document.createElement('div');
+      let paragraphEl = document.createElement('p');
+      paragraphEl.style.display = 'none';
+      paragraphEl.textContent = el;
+      divEl.appendChild(paragraphEl);
+      contentElements.appendChild(divEl);
+   }
+
+   let reveal = function (e) {
+      // e.target.querySelector('p').style.display = 'block';
+      // OR
+      e.target.firstChild.style.display ='block';
+   }
+
+   contentElements.addEventListener('click', reveal);
 }
