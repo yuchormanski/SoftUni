083cdb9bcd51edda886f71dd7303403efd54f3b5
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-11 08:01:59 +0200

    done

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/03. Locked-Profile/app.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let usersList = document.querySelectorAll('.profile');
+
+    for (let user of usersList) {
+        user.children[10].addEventListener('click', showMore);
+    }
+
+    function showMore(e) {
+        let statusUnlock = e.currentTarget.parentNode.children[4];
+        let hiddenDiv = e.currentTarget.parentNode.children[9];
+
+        if (statusUnlock.checked) {
+            if (e.currentTarget.textContent == 'Show more') {
+                hiddenDiv.style.display = 'block';
+                e.currentTarget.textContent = 'Hide it'
+            } else {
+                hiddenDiv.style.display = 'none';
+                e.currentTarget.textContent = 'Show more'
+            }
+        }
+    }
 }
