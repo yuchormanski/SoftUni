4caccca7457c100066f8c3d0c904c3af0f3b4f06
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-11 22:31:06 +0200

    comments added

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,13 +1,14 @@
 function addItem() {
 
-    const menuEl = document.getElementById('menu');
-    const textEl = document.getElementById('newItemText');
-    const valueEl = document.getElementById('newItemValue');
+    const menuEl = document.getElementById('menu'); // get ref to <SELECT> element
+    const textEl = document.getElementById('newItemText');  // ref to text input field
+    const valueEl = document.getElementById('newItemValue'); // ref to value field
 
-    const newOption = document.createElement('option');
-    newOption.textContent = textEl.value;
-    newOption.value = valueEl.value;
-    menuEl.appendChild(newOption);
-    textEl.value = '';
+    const newOption = document.createElement('option'); // create new <OPTION> element
+    newOption.textContent = textEl.value; // append text to it from text input field
+    newOption.value = valueEl.value; // add value to it from value input field
+    menuEl.appendChild(newOption); // append new created element to <SELECT> 
+
+    textEl.value = ''; //clear input fields after previous operations
     valueEl.value = '';
 }
