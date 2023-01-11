a97a050a0d19242149d4353f36fb24230fadcfbb
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-11 22:26:03 +0200

    done

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const menuEl = document.getElementById('menu');
+    const textEl = document.getElementById('newItemText');
+    const valueEl = document.getElementById('newItemValue');
+
+    const newOption = document.createElement('option');
+    newOption.textContent = textEl.value;
+    newOption.value = valueEl.value;
+    menuEl.appendChild(newOption);
+    textEl.value = '';
+    valueEl.value = '';
 }
