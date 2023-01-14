30391a35003d3f9d6dbefba36ca0dcf7fcee277c
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-14 11:00:30 +0200

    done!

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,28 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    const inputSelection = document.getElementById('inputUnits');
+    const userInput = document.getElementById('inputDistance');
+    const buttonEl = document.getElementById('convert');
+    const outputSelection = document.getElementById('outputUnits')
+    const result = document.getElementById('outputDistance');
+
+    buttonEl.addEventListener('click', convertIt);
+
+    function convertIt() {
+
+        const units = {
+            mm: 0.001,
+            cm: 0.01,
+            m: 1,
+            km: 1000,
+            mi: 1609.34,
+            ft: 0.3048,
+            yrd: 0.9144,
+            in: 0.0254
+        }
+        if (!isNaN(userInput.value) && userInput.value != '') {
+            let conversion = (userInput.value * units[inputSelection.value]) / units[outputSelection.value];
+            result.value = conversion;
+        }
+    }
 }
