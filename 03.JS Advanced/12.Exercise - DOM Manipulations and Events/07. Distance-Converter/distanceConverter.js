40ab584ef994b2a59f546e1ca5acf0ce7140b0de
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-14 11:56:43 +0200

    comments added

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
@@ -31,17 +31,17 @@
 // fixed result to 7 digit after decimal point
 function attachEventsListeners() {
 
-    const inputSelection = document.getElementById('inputUnits');
-    const userInput = document.getElementById('inputDistance');
-    const buttonEl = document.getElementById('convert');
-    const outputSelection = document.getElementById('outputUnits')
-    const result = document.getElementById('outputDistance');
+    const inputSelection = document.getElementById('inputUnits');  // get 'FROM' units value
+    const userInput = document.getElementById('inputDistance');  // get user input value
+    const buttonEl = document.getElementById('convert');    // get button 
+    const outputSelection = document.getElementById('outputUnits')  // get 'To ...' units value
+    const result = document.getElementById('outputDistance');  // get result field
 
-    buttonEl.addEventListener('click', convertIt);
+    buttonEl.addEventListener('click', convertIt);  //add listener on button
 
     function convertIt() {
 
-        const units = {
+        const units = {  //create object with values equal to 1m
             mm: 0.001,
             cm: 0.01,
             m: 1,
@@ -51,9 +51,9 @@ function attachEventsListeners() {
             yrd: 0.9144,
             in: 0.0254
         }
-        if (!isNaN(userInput.value) && userInput.value != '') {
+        if (!isNaN(userInput.value) && userInput.value != '') { // IF user input is NUMBER and is not empty string
             let conversion = (userInput.value * units[inputSelection.value]) / units[outputSelection.value];
-            result.value = parseFloat(conversion.toFixed(7));
+            result.value = parseFloat(conversion.toFixed(7)); //print result
         }
     }
 }
