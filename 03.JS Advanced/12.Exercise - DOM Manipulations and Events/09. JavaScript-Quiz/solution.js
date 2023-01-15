495f8b876c36bf0b914ef868301a56eb71126ee5
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-15 06:47:45 +0200

    inProgress

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+
+  const rightAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents']
+  let right = 0;
+  let wrong = 0;
+  const sectionElements = Array.from(document.querySelectorAll('section'));
+
+  for (let i = 0; i < sectionElements.length; i++) {
+   
+    const buttons = sectionElements[i].querySelectorAll('p');
+
+    for (let button of buttons) {
+      button.addEventListener('click', (e) =>{
+        let answer = e.currentTarget.textContent
+        if(rightAnswers.includes(answer)){
+          right++;
+        }else{
+          wrong++;
+        }
+        sectionElements[i].style.display = 'none';
+        sectionElements[i+1].style.display ='block';
+        console.log(sectionElements[i]);
+         console.log(right, wrong);
+      })
+    }
+    
+  }
+
 }
+
+
