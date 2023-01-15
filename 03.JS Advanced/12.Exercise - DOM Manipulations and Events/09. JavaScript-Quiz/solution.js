dcb69991661d134202f1b4ec66707b2e6937e877
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-15 15:28:54 +0200

    done!

--- 03.JS Advanced/12.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
+++ 03.JS Advanced/12.Exercise - DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
@@ -1,31 +1,37 @@
 function solve() {
 
   const rightAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents']
-  let right = 0;
-  let wrong = 0;
   const sectionElements = Array.from(document.querySelectorAll('section'));
+  const resultElement = document.querySelector('#results');
+  let right = 0;
+  let finalAnswer = 'You are recognized as top JavaScript fan!';
 
   for (let i = 0; i < sectionElements.length; i++) {
-   
+
     const buttons = sectionElements[i].querySelectorAll('p');
 
     for (let button of buttons) {
-      button.addEventListener('click', (e) =>{
+      button.addEventListener('click', (e) => {
         let answer = e.currentTarget.textContent
-        if(rightAnswers.includes(answer)){
+        if (rightAnswers.includes(answer)) {
           right++;
-        }else{
-          wrong++;
         }
-        sectionElements[i].style.display = 'none';
-        sectionElements[i+1].style.display ='block';
-        console.log(sectionElements[i]);
-         console.log(right, wrong);
-      })
-    }
-    
-  }
 
+        if (i + 1 < sectionElements.length) {
+          sectionElements[i].style.display = 'none';
+          sectionElements[i + 1].style.display = 'block';
+        } else {
+          sectionElements[i].style.display = 'none';
+          resultElement.style.display = 'block';
+
+          if (right < sectionElements.length) {
+            finalAnswer = `You have ${right} right answers`
+          }
+          resultElement.querySelector('h1').textContent = finalAnswer;
+        }
+      });
+    }
+  }
 }
 
 
