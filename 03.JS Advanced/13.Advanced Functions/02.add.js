29b7d6fccb7904d1fb15361c343edd1d9f1df602
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-20 13:20:37 +0200

    up

--- 03.JS Advanced/13.Advanced Functions/02.add.js
+++ 03.JS Advanced/13.Advanced Functions/02.add.js
@@ -1,8 +1,13 @@
 function add(num) {
 
-    let result = (n) => num + n;
+    // let result = (n) => num + n;
+    // return result
+
+    let result = function sum(n) {
+        return num + n;
+    }
     return result
 }
 let add5 = add(5);
 console.log(add5(2));
-console.log(add5(3));
+console. log(add5(3));
