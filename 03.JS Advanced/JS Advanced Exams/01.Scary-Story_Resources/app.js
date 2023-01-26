f31886bfa1c6762d01b6b2212439892c77f806d8
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-26 08:28:39 +0200

    up

--- 03.JS Advanced/JS Advanced Exams/01.Scary-Story_Resources/app.js
+++ 03.JS Advanced/JS Advanced Exams/01.Scary-Story_Resources/app.js
@@ -10,85 +10,100 @@ function solve() {
   const btn = document.getElementById('form-btn');
 
   const previewList = document.getElementById('preview-list');
-  previewList.addEventListener('click', editSaveDelete);
+
+
+  //creating elements
+  const newLi = document.createElement('li');
+  newLi.className = "story-info";
+  const newArticle = document.createElement('article');
+  const headerName = document.createElement('h4');
+  const agePar = document.createElement('p')
+  const titlePar = document.createElement('p');
+  const genrePar = document.createElement('p');
+  const storyPar = document.createElement('p');
+  
+  //redaction buttons
+
+  const saveBtn = document.createElement('button');
+  saveBtn.className = 'save-btn';
+  saveBtn.textContent = 'Save story';
+  saveBtn.setAttribute('disabled', '');
+
+  const editBtn = document.createElement('button');
+  editBtn.className = 'edit-btn';
+  editBtn.textContent = 'Edit story';
+  
+  const delBtn = document.createElement('button');
+  delBtn.className = 'delete-btn';
+  delBtn.textContent = 'Delete story';
+  
+  
+
 
   btn.addEventListener('click', preview);
 
   function preview(event) {
 
-    // if (firstName.value !== '' && firstName.value !== ' ' &&
-    //   lastName.value !== '' && lastName.value !== ' ' &&
-    //   age.value !== '' && age.value !== ' ' &&
-    //   storyTitle.value !== '' && storyTitle.value !== ' ' &&
-    //   userStory.value !== '' && userStory.value !== ' ') {
+    if (firstName.value !== '' && firstName.value !== ' ' &&
+      lastName.value !== '' && lastName.value !== ' ' &&
+      age.value !== '' && age.value !== ' ' &&
+      storyTitle.value !== '' && storyTitle.value !== ' ' &&
+      userStory.value !== '' && userStory.value !== ' ') {
 
-    // creatin user object
-    const obj = {
-      name: `${firstName.value} ${lastName.value}`,
-      age: age.value,
-      storyTitle: storyTitle.value,
-      genre: genre.value,
-      story: userStory.textContent
+      // creating user object
+      const obj = {
+        userFirstName: firstName.value,
+        userLastName: lastName.value,
+        age: age.value,
+        storyTitle: storyTitle.value,
+        genre: genre.value,
+        story: userStory.textContent
+      }
+
+      //clear input fields
+      firstName.value = '';
+      lastName.value = '';
+      age.value = '';
+      storyTitle.value = '';
+      userStory.value = '';
+
+      headerName.textContent = `Name: ${obj.userFirstName} ${obj.userLastName}`;
+      agePar.textContent = `Age: ${obj.age}`;
+      titlePar.textContent = `Title: ${obj.storyTitle}`;
+      genrePar.textContent = `Genre: ${obj.genre}`;
+      storyPar.textContent = `${obj.story}`;
+      newArticle.append(headerName, agePar, titlePar, genrePar, storyPar);
+
+
+      //disabling publish btn
+      btn.setAttribute('disabled', '');
     }
-
-    //clear input fields
-    firstName.value = '';
-    lastName.value = '';
-    age.value = '';
-    storyTitle.value = '';
-    userStory.value = '';
-
-
-    //creating elements
-    const newLi = document.createElement('li');
-    newLi.className = "story-info";
-
-    
-    const newArticle = document.createElement('article');
-    
-    const headerName = document.createElement('h4');
-    headerName.textContent = `Name: ${obj.name}`;
-    const agePar = document.createElement('p')
-    agePar.textContent = `Name: ${obj.age}`;
-    const titlePar = document.createElement('p');
-    titlePar.textContent = `Name: ${obj.storyTitle}`;
-    const genrePar = document.createElement('p');
-    genrePar.textContent = `Name: ${obj.genre}`;
-    const storyPar = document.createElement('p');
-    storyPar.textContent = `Name: ${obj.story}`;
-    
-    newArticle.append(headerName, agePar, titlePar, genrePar, storyPar);
-    
-    
-    //redaction buttons
-    
-    const saveBtn = document.createElement('button');
-    saveBtn.className = 'save-btn';
-    saveBtn.textContent = 'Save story';
-    
-    const editBtn = document.createElement('button');
-    editBtn.className = 'edit-btn';
-    editBtn.textContent = 'Edit story';
-    
-    const delBtn = document.createElement('button');
-    delBtn.className = 'delete-btn';
-    delBtn.textContent = 'Delete story';
-    
     newLi.append(newArticle, saveBtn, editBtn, delBtn);
-    
     previewList.append(newLi);
 
-    //disabling publish btn
-    btn.setAttribute('disabled','');
+        
+    liElement = document.querySelector('li.story-info');
+
+    liElement.addEventListener('click', (e) =>{
+      const liName =  liElement.querySelector('h4');
+      const info =  liElement.querySelectorAll('p');
+      let butArray = Array.from(liElement.querySelectorAll('button'));
+      console.log(liElement);
+      if (e.target.tagName === 'BUTTON') {
+        btn.removeAttribute('disabled', '');
+      }
     
     
-    // }
-
-  }
-
-  function editSaveDelete(event){
-    if(event.target.tagName === 'BUTTON'){
-      btn.removeAttribute('disabled','');
-    }
+      if (e.target === butArray[1]) {
+        // let [firstN, lastN] = headerName.textContent.split('Name: ');
+        firstName.value = liName.textContent.split('Name: ');
+        
+        // lastName.value = obj.userLastName;
+        // age.value = obj.age;
+        // storyTitle.value = obj.storyTitle;
+        // userStory.value = obj.story;
+        e.target.setAttribute('disabled', '')
+      }
+    });
   }
 }
