5b1884b96e8bba97d9ec80a83e2d0a113ef30dff
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-30 19:33:42 +0200

    up

--- 03.JS Advanced/14.Exercise - Advanced Functions/01.sortArray.js
+++ 03.JS Advanced/14.Exercise - Advanced Functions/01.sortArray.js
@@ -1,12 +1,17 @@
-function sorting(...input) {
-    const [numArray, criteria] = input;
+// function sorting(...input) {
+//     const [numArray, criteria] = input;
 
-    if (criteria === 'asc') {
-        numArray.sort((a, b) => a - b);
-    } else {
-        numArray.sort((a, b) => b - a);
-    }
-    return numArray;
+//     if (criteria === 'asc') {
+//         numArray.sort((a, b) => a - b);
+//     } else {
+//         numArray.sort((a, b) => b - a);
+//     }
+//     return numArray;
+// }
+// sorting([14, 7, 17, 6, 8], 'asc')
+// sorting([14, 7, 17, 6, 8], 'desc')
+
+function sorting(...input) {
+    return input[1] === 'asc' ? input[0].sort((a, b) => a - b) : input[0].sort((a, b) => b - a);
 }
-sorting([14, 7, 17, 6, 8], 'asc')
-sorting([14, 7, 17, 6, 8], 'desc')
+console.log(sorting([14, 7, 17, 6, 8], 'asc'));
