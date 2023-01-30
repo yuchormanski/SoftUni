3cb1661d5e80defa3b82a68b24c35954c674a04c
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-01-31 00:04:19 +0200

    up

--- 03.JS Advanced/14.Exercise - Advanced Functions/07. Simple Calculator/app.js
+++ 03.JS Advanced/14.Exercise - Advanced Functions/07. Simple Calculator/app.js
@@ -1,7 +1,24 @@
 function calculator() {
-    // TODO:
+
+    return {
+        init: (sel1, sel2, res) => {
+            first = document.querySelector(sel1);
+            second = document.querySelector(sel2);
+            result = document.querySelector(res);
+        },
+        add: () => {
+            let output = Number(first.value) + Number(second.value);
+            result.value = output;
+        },
+        subtract: () => {
+            let output = Number(first.value) - Number(second.value);
+            result.value = output;
+        }
+    }
 }
 
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
 
 
 
