db413335858295c81ac258eba2fa159033682a66
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-02 23:37:39 +0200

    up

---
+++ 03.JS Advanced/16.Exercise - Unit Testing and Error Handling/testing/index.js
@@ -0,0 +1,58 @@
+/* function sum(n1,n2){
+    return Number(n1) + Number(n2);
+}
+ */
+//------------------------
+// task 2.	Even or Odd
+
+/* function isOddOrEven(string) {
+    if (typeof(string) !== 'string') {
+        return undefined;
+    }
+    if (string.length % 2 === 0) {
+        return "even";
+    }
+
+    return "odd";
+} */
+//---------------------
+//task 3 3.	Char Lookup
+
+/* function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+    return string.charAt(index);
+} */
+//-------------------
+//task 4.Math Enforcer
+/* 
+let mathEnforcer = {
+    addFive: function (num) {
+        if (typeof(num) !== 'number') {
+            return undefined;
+        }
+        return num + 5;
+    },
+    subtractTen: function (num) {
+        if (typeof(num) !== 'number') {
+            return undefined;
+        }
+        return num - 10;
+    },
+    sum: function (num1, num2) {
+        if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+            return undefined;
+        }
+        return num1 + num2;
+    }
+};
+ */
+
+
+// module.exports = isOddOrEven;
+// module.exports = lookupChar;
+// module.exports = mathEnforcer;
