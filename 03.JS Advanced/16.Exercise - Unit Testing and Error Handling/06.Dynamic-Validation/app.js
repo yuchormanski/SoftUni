f5c0297aaa7529a5c2e6a648f5e4ff9fd13a8ea6
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-03 00:20:50 +0200

    done!

--- 03.JS Advanced/16.Exercise - Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ 03.JS Advanced/16.Exercise - Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    const inputField = document.getElementById('email');
+    inputField.addEventListener('change', (e) => {
+        const regex = /([a-z]+)(@)([a-z]+)(\.)([a-z]+)/g;
+        let email = inputField.value;
+        if (!email.match(regex)) {
+            inputField.className = 'error';
+        } else {
+            inputField.className = '';
+        }
+    })
 }
