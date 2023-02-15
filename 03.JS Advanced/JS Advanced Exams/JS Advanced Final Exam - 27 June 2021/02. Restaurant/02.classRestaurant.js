43f0cbaeeb12f28d94cdfad359ab9ba54c5c5a9d
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-15 08:52:38 +0200

    up

---
+++ 03.JS Advanced/JS Advanced Exams/JS Advanced Final Exam - 27 June 2021/02. Restaurant/02.classRestaurant.js
@@ -0,0 +1,92 @@
+class Restaurant {
+    constructor(budgetMoney) {
+        this.budgetMoney = budgetMoney;
+        this.menu = {};
+        this.stockProducts = {};
+        this.history = [];
+    }
+
+    loadProducts(data) {
+        data.forEach(line => {
+            let [productName, productQuantity, productTotalPrice] = line.split(' ');
+            productQuantity = Number(productQuantity);
+            productTotalPrice = Number(productTotalPrice);
+
+            if (this.budgetMoney >= productTotalPrice) {
+                if (!this.stockProducts[productName]) {
+                    this.stockProducts[productName] = { quantity: productQuantity };
+                }
+                else {
+                    this.stockProducts[productName].quantity += productQuantity;
+                }
+                this.budgetMoney -= productTotalPrice;
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            } else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
+            }
+        });
+        return this.history.join('\n');
+
+    }
+
+    addToMenu(meal, neededProducts, price) {
+        if (this.menu[meal]) {
+            return `The ${meal} is already in the our menu, try something different.`
+        }
+        price = Number(price);
+        this.menu[meal] = { products: neededProducts, price };
+        let count = Object.keys(this.menu).length;
+        if (count === 1) {
+            return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        } else {
+            return `Great idea! Now with the ${meal} we have ${count} meals in the menu, other ideas?`
+        }
+    }
+
+    showTheMenu() {
+        const menuArray = []
+        if (Object.keys(this.menu).length === 0) {
+            return 'Our menu is not ready yet, please come later...';
+        }
+        for (let meal in this.menu) {
+            //{meal} - $ {meal price}
+            menuArray.push(`${meal} - $ ${this.menu[meal].price}`)
+        }
+        return menuArray.join('\n');
+    }
+
+    makeTheOrder(meal) {
+        if (!this.menu[meal]) {
+            return `There is not ${meal} yet in our menu, do you want to order something else?`
+        }
+        for (let line of this.menu[meal].products) {
+            let [product, qty] = line.split(' ');
+            qty = Number(qty);
+            if (this.stockProducts[product].quantity < qty) {
+                return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
+            }
+        }
+        for (let line of this.menu[meal].products) {
+            let [product, qty] = line.split(' ');
+            qty = Number(qty);
+            this.stockProducts[product].quantity -= qty;
+        }
+        return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}.`
+    }
+}
+
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
+
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+// console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
+
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.showTheMenu());
+
+let kitchen = new Restaurant(1000);
+kitchen.loadProducts(['Yogurt 30 3', 'Honey 50 4', 'Strawberries 20 10', 'Banana 5 1']);
+kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 21'], 9.99);
+console.log(kitchen.makeTheOrder('frozenYogurt'));
+
