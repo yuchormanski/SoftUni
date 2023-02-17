0b952225bc4a933262dcc54140e2151cb982a6fe
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-17 07:05:04 +0200

    update

--- 00.Docs/#elementCreator-Shortcut.js
+++ 00.Docs/#elementCreator-Shortcut.js
@@ -1,10 +1,20 @@
-function creator(type, attribute, attrValue, textCont) {
-    const element = document.createElement(type);
-    if (attribute != '') {
-        element[attribute] = attrValue;
+
+// element type, element attribute - if amy, attribute value- if attribute, element text - if any
+
+function creator(elType, elAttribute, attrValue, elementText) {
+    const element = document.createElement(elType);
+    if (elAttribute != '') {
+        element[elAttribute] = attrValue;
     }
-    if (textCont != '') {
-        element.innerText = textCont;
+    if (elementText != '') {
+        element.innerText = elementText;
     }
+    return element;
+}
+
+function creator(elType, elAttribute, attrValue, elementText) {
+    const element = document.createElement(elType);
+    elAttribute ? element[elAttribute] = attrValue : null;
+    elementText ? element.innerText = elementText : null;
     return element;
 }
