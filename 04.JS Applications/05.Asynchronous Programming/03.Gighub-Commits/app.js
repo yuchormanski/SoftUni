c4023baa5fd90b940b6bb69b062b8666e0936343
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-24 15:49:36 +0200

    can't get error status

--- 04.JS Applications/05.Asynchronous Programming/03.Gighub-Commits/app.js
+++ 04.JS Applications/05.Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,29 @@
 function loadCommits() {
     // Try it with Fetch API
-    console.log('TODO...');
+    const nameField = document.getElementById('username');
+    const repo = document.getElementById('repo');
+    const ulEle = document.getElementById('commits');
+    const baseURL = `https://api.github.com/repos/${nameField.value}/${repo.value}/commits`
+    ulEle.innerHTML = '';
+
+    fetch(baseURL)
+        .then((result) => result.json())
+        .then((data) => {
+            data.forEach(element => {
+                const currentText = `${element.commit.author.name}: ${element.commit.message}`;
+                const li = creator('li', currentText);
+                ulEle.appendChild(li);
+            });
+        })
+        .catch((error) => {
+            const currentText = `Error: ${error.status} (Not Found)`;
+            const li = creator('li', currentText);
+            ulEle.appendChild(li);
+        })
+}
+
+function creator(type, text) {
+    const element = document.createElement(type);
+    element.innerText = text;
+    return element;
 }
