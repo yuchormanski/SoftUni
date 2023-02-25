975d659bdaf69dd0d6634fa108a0779c3c92b072
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-02-25 14:50:08 +0200

    done!

--- 04.JS Applications/06.Exercise - Asynchronous Programming/02.Bus-Schedule/app.js
+++ 04.JS Applications/06.Exercise - Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
+    const url = 'http://localhost:3030/jsonstore/bus/schedule';
+    let infoBox = document.querySelector('.info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let nextStop = 'depot';
+    let stopName = '';
 
     function depart() {
-        console.log('Depart TODO...');
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+        fetch(`${url}/${nextStop}`)
+            .then(res => res.json())
+            .then(data => {
+                let { name, next } = data;
+                nextStop = next;
+                stopName = name;
+                infoBox.innerText = `Next stop ${stopName}`;
+            })
+            .catch(error => {
+                infoBox.innerText = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.innerText = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
