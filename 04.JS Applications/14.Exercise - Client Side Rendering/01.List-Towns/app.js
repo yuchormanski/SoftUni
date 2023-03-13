3a3c080af958829d74fcdcffdaad0027092544c9
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-03-13 19:32:57 +0200

    done!

--- 04.JS Applications/14.Exercise - Client Side Rendering/01.List-Towns/app.js
+++ 04.JS Applications/14.Exercise - Client Side Rendering/01.List-Towns/app.js
@@ -1,4 +1,5 @@
-import { html, render } from './node_modules/lit-html/lit-html.js'
+// import { html, render } from './node_modules/lit-html/lit-html.js'
+import { html, render } from 'https://unpkg.com/lit-html?module'
 const root = document.getElementById('root');
 const form = document.querySelector('form');
 form.querySelector('button#btnLoadTowns').addEventListener('click', createList);
@@ -8,12 +9,15 @@ function createList(e) {
     e.preventDefault();
     const formData = new FormData(form);
     const data = Object.values(Object.fromEntries(formData))[0].split(', ');
+    const ul = document.createElement('ul');
+    root.appendChild(ul);
 
-    const townTemplate = (town) => html`
-    <ul>
+    if (data != '') {
+        const townTemplate = (town) => html`
         <li>${town}</li>
-    </ul>
     `;
-    render(data.map(townTemplate), root)
+        render(data.map(townTemplate), ul)
+        form.reset();
+    }
 }
 
