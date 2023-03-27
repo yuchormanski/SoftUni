35cea4253158ee6ef37c19a0e56de83d4fc882b5
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-03-27 23:21:29 +0300

    reorder

--- 000.Shortcuts + exportable/requests/data.js
+++
@@ -1,6 +0,0 @@
-import { get } from 'api.js';
-
-
-export async function getSomethingForGet('url for get post') {
-    return get('url for get post');
-}
--- 000.Shortcuts + exportable/requests/post.js
+++
@@ -1,6 +0,0 @@
-import { post } from "./api.js";
-
-export async function post({data}) {
-    const postData = await post('url for post', { data });
- // do something as load  all items including new-posted-dat
-}
