f6910a10fa74bd9ed003921383627d22fdb24fee
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-04-17 11:20:29 +0300

    add express-handlebars

--- JS Back-End/06.Workshop - ExpressJS and Templating/Cubicle/src/index.js
+++ JS Back-End/06.Workshop - ExpressJS and Templating/Cubicle/src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
-const config = require('./config.js');
+const config = require('./config'); // ако файла се казва index, ще бъде автоматично импортнат иначе трябва да се обяви.
+// const config = require('./config/index.js'); // read above
 
 const app = express();
 
+// const setupViewEngine = require('./config/viewEngine.js');
+// setupViewEngine(app)
+
+//same as above
+require('./config/viewEngine.js')(app);
+
+
+
 app.get('/', (req, res) => {
-    app.send('Home page');
+    res.render('home');
 });
 
-app.listen(config.PORT, () => {console.log(`Server is running on port ${config.PORT}...`)});
+app.listen(config.PORT, () => { console.log(`Server is running on port ${config.PORT}...`) });
 
