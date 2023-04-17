10689b03c76ff28f4c4664c409ad2cdd59ca3b18
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-04-17 09:19:17 +0300

    up

--- JS Back-End/05.ExpressJS and Templating/index.js
+++ JS Back-End/05.ExpressJS and Templating/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const logger = require('./logger.js');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 5000;
 
-app.use(express.static('public'))
+
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views'); // views е директория по подразбиране и ако се казва така , може да не се сетва. В противен случай трябва да се зададе име и път до директорията
+// app.set('tems', './templates'); // това да се пробва
+
+
+app.use(express.static(`${__dirname}/public`));
 
 const validateId = (req, res, next) => {  // middleware
-    const id = Number(req.params.id); // ще върне NaN , ако не е число
-    // може да се направи каквато и да е валидация
-    if (!id) {  // NaN  е falsy
-        // return res.send('Invalid cat id number');
+    const catId = Number(req.params.catId); // ще върне NaN , ако не е число; може да се направи каквато и да е валидация
+    if (!catId) {  // NaN  е falsy;   // return res.send('Invalid cat id number');
         return res.redirect('/404')
     }
+    req.catId = catId; // добавяме id-то в рикуест обекта
     next();
 };
 
@@ -30,16 +37,42 @@ app.get('/404', (req, res) => {
     `)
 });
 
-app.get('/', (req, res) => {    // get request to server
+app.get('/old', (req, res) => {    // get request to server
     res.status(200); // is not require. it will be set automatically
-    res.send(`<h1>Hello from Express!</h1>
-            <a href="/cats" >cats</a>`);
+    res.send(
+        `<html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+        </head>
+        <body>
+            <h1>Hello from Express!</h1>
+            <ul>
+                <li>
+                    <a href="/cats" >cats</a>
+                </li>
+            </ul>
+            <img src="/jpg/dog.jpg" alt="" width="200">
+        </body>
+    </html>`
+    );
 });
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
 app.get('/cats', (req, res) => {
-    res.send(`<h1>Cats page</h1>
-    <a href="/" >home</a>
-    `);
+    // res.send(`<h1>Cats page</h1>
+    // <a href="/" >home</a>
+    // `);
+
+    const cats = [
+        { name: 'Lora', breed: 'Vizsla', age: 6 },
+        { name: 'Jetta', breed: 'Setter', age: 12 },
+        { name: 'Heidy', breed: 'Shepard dog', age: 8 },
+    ];
+
+    res.render('cats', { cats });
 });
 
 app.get('/cats/1', (req, res) => {  // ще отвори download диалог
@@ -47,18 +80,19 @@ app.get('/cats/1', (req, res) => {  // ще отвори download диалог
 });
 
 app.get('/cats/11', (req, res) => {  // ще отвори фаила в нов прозорец
-    res.sendFile('./dog.jpg', { root: __dirname })
+    res.sendFile('./public/jpg/dog.jpg', { root: __dirname })
 });
 
 
-app.get('/cats/:id', validateId, (req, res) => {        //задаване на параметри
-    const id = req.params.id;
-    res.send(`<h1>Individual cat page = ${id}</h1>
-                <a href="/" >home</a>`);
-    console.log(req.params);
+app.get('/cats/:catId', validateId, (req, res) => {        //задаване на параметри
+    // const id = req.params.id;
+    // res.send(`<h1>Individual cat page = ${req.params.id}</h1>
+    //             <a href="/" >home</a>`);
+    // console.log(req.params);
+    res.render('cat', { id: req.params.catId, isOdd: req.catId % 2 != 0});  //req.catId е добавено  middleware
 });
 
-app.get('/cats/:id/:foodId', validateId,  (req, res) => {        //задаване на параметри + chaining
+app.get('/cats/:id/:foodId', validateId, (req, res) => {        //задаване на параметри + chaining
     const id = req.params.id;
     const foodId = req.params.foodId;
     res.send(`<h1>Individual cat page = ${id}/${foodId}%</h1>
