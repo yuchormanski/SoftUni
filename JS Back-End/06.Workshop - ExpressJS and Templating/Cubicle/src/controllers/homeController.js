835f9b2e6f63cca0046dc04b3210199e3cac7e74
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-04-18 08:32:08 +0300

    add search

--- JS Back-End/06.Workshop - ExpressJS and Templating/Cubicle/src/controllers/homeController.js
+++ JS Back-End/06.Workshop - ExpressJS and Templating/Cubicle/src/controllers/homeController.js
@@ -1,7 +1,22 @@
 const db = require('../db.json');
 
 exports.getHomePage = (req, res) => {
-    res.render('index', { cubes: db.cubes });
+    const { search, from: difficultyFrom, to: difficultyTo } = req.query;
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (difficultyFrom) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
+    }
+
+    if (difficultyTo) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
+    }
+
+    res.render('index', { cubes , search, difficultyFrom, difficultyTo});
 };
 
 exports.getAboutPage = (req, res) => {
