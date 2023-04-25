f447ac7e73e6b58e77eb3a6568c81e7a1da0c9ca
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-04-25 20:20:12 +0300

    accessory model

---
+++ JS Back-End/08.Workshop - MongoDB and Mongoose/src/models/Accessoary.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        // validation
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 150,
+    },
+
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+model.exports = Accessory;
