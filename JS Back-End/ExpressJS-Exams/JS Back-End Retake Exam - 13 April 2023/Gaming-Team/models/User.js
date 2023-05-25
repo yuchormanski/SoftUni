6ef205147f388595f27c5c401a72915a005f7c3e
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-05-26 00:29:18 +0300

    up

--- JS Back-End/ExpressJS-Exams/JS Back-End Retake Exam - 13 April 2023/Gaming-Team/models/User.js
+++ JS Back-End/ExpressJS-Exams/JS Back-End Retake Exam - 13 April 2023/Gaming-Team/models/User.js
@@ -1,13 +1,32 @@
 const { Schema, model } = require('mongoose');
 
 
-//TODO: add User properties and validation according to assignment requirements
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true , minLength: [3, 'Username should be at least 3 characters long']},
-    hashedPassword: { type: String, required: true }
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true , 
+        minLength: [5, 'Username should be at least 5 characters long']
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true , 
+        minLength: [10, 'Email should be at least 10 characters long']
+    },
+    hashedPassword: { 
+        type: String, 
+        required: true 
+    }
 });
 
 userSchema.index({username:1}, {
+    collation: {
+        locale:'en',
+        strength: 2
+    }
+})
+userSchema.index({email:1}, {
     collation: {
         locale:'en',
         strength: 2
