29ab31fdb3bdd1097c916d3f36dc84cc32d05635
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-06-01 08:54:59 +0300

    created home page

--- JS Back-End/ExpressJS-Exams/JS Back-End Exam preparation- януари 2021/SoftUni Wiki/controllers/homeController.js
+++ JS Back-End/ExpressJS-Exams/JS Back-End Exam preparation- януари 2021/SoftUni Wiki/controllers/homeController.js
@@ -1,10 +1,24 @@
+const { getHomeArticles } = require('../services/articleService.js');
+
 const homeController = require('express').Router();
 
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
+    const lastThree = await getHomeArticles().lean();
+    const homeArticles = []
+    lastThree.forEach(x => {
+        const current = {};
+        current.title = x.title;
+        current.content = x.content.split(' ').slice(0,49).join(' ');
+        homeArticles.push(current);
+    }   
+    )
+
+    console.log(homeArticles);
     res.render('home', {
         pageTitle: 'Home page',  //if needed
-        user: req.user       //if needed
+        user: req.user,       //if needed
+        homeArticles
     });
 });
 
--- JS Back-End/ExpressJS-Exams/JS Back-End Exam preparation- януари 2021/SoftUni Wiki/services/articleService.js
+++ JS Back-End/ExpressJS-Exams/JS Back-End Exam preparation- януари 2021/SoftUni Wiki/services/articleService.js
@@ -1,5 +1,6 @@
 const Article = require('../models/Article.js');
 
-
+exports.getHomeArticles = () => Article.find().sort({createdAt: -1}).limit(3);
 
 exports.createArticle = (data) => Article.create(data);
+
