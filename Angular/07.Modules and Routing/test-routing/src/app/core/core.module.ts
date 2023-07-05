3a94cc28a87e38f9f2ff6ca738fc80664124e1ce
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-07-05 06:34:23 +0300

    Merge branch 'main' of https://github.com/yuchormanski/SoftUni

--- Angular/07.Modules and Routing/test-routing/src/app/core/core.module.ts
+++ Angular/07.Modules and Routing/test-routing/src/app/core/core.module.ts
@@ -5,10 +5,21 @@ import { FooterComponent } from './footer/footer.component';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [NavigationComponent, FooterComponent, GlobalLoaderComponent],
+  declarations: [
+    NavigationComponent,
+    FooterComponent,
+    GlobalLoaderComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, SharedModule, RouterModule],
-  exports: [NavigationComponent, FooterComponent, GlobalLoaderComponent],
+  exports: [
+    NavigationComponent,
+    FooterComponent,
+    GlobalLoaderComponent,
+    HeaderComponent,
+  ],
 })
 export class CoreModule {}
