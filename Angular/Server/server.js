df4376f97a6041fbfa5593bef9172c8d1eac2d49
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-07-21 00:48:08 +0300

    up

--- Angular/Server/server.js
+++ Angular/Server/server.js
@@ -1426,53 +1426,368 @@
     users: {
       "35c62d76-8152-4626-8712-eeb96381bea8": {
         email: "peter@abv.bg",
-        firstName: "Peter",
-        lastName: "Petrov",
+        username: "Peter",
         hashedPassword:
           "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
       },
       "847ec027-f659-4086-8032-5173e2f9c93a": {
-        email: "john@abv.bg",
+        email: "george@abv.bg",
+        username: "George",
         hashedPassword:
           "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1",
+      },
+      "60f0cf0b-34b0-4abd-9769-8c42f830dffc": {
+        email: "admin@abv.bg",
+        username: "Admin",
+        hashedPassword:
+          "fac7060c3e17e6f151f247eacb2cd5ae80b8c36aedb8764e18a41bbdc16aa302",
       },
     },
     sessions: {},
   };
   var seedData = {
-    games: {
-      "ff436770-76c5-40e2-b231-77409eda7a61": {
+    recipes: {
+      "3987279d-0ad4-4afb-8ca9-5b256ae3b298": {
         _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        title: "CoverFire",
-        category: "Action",
-        maxLevel: "70",
-        imageUrl: "/images/CoverFire.png",
-        summary:
-          "Best action shooter game, easy controls, realistic 3D graphics and fun offline missions. Get your best shooting gun and take to action!",
-        _createdOn: 1617194128618,
+        name: "Easy Lasagna",
+        img: "assets/lasagna.jpg",
+        ingredients: [
+          "1 tbsp Ingredient 1",
+          "2 cups Ingredient 2",
+          "500 g  Ingredient 3",
+          "25 g Ingredient 4",
+        ],
+        steps: ["Prepare ingredients", "Mix ingredients", "Cook until done"],
+        _createdOn: 1613551279012,
       },
-      "1840a313-225c-416a-817a-9954d4609f7c": {
+      "8f414b4f-ab39-4d36-bedb-2ad69da9c830": {
         _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-        title: "MineCraft",
-        category: "Arcade",
-        maxLevel: "250",
-        imageUrl: "/images/MineCraft.png",
-        summary:
-          "Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.",
-        _createdOn: 1617194210928,
+        name: "Grilled Duck Fillet",
+        img: "assets/roast.jpg",
+        ingredients: [
+          "500 g  Ingredient 1",
+          "3 tbsp Ingredient 2",
+          "2 cups Ingredient 3",
+        ],
+        steps: ["Prepare ingredients", "Mix ingredients", "Cook until done"],
+        _createdOn: 1613551344360,
       },
-      "126777f5-3277-42ad-b874-76d043b069cb": {
+      "985d9eab-ad2e-4622-a5c8-116261fb1fd2": {
         _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
-        title: "Zombie Lang",
-        category: "Vertical Shooter",
-        maxLevel: "100",
-        imageUrl: "/images/ZombieLang.png",
-        summary:
-          "With it’s own unique story, set between the events of the first movie, Zombieland: Double Tap- Road Trip is a ridiculously fun top-down twin-stick shooter featuring local co-op multiplayer for up to four players. Play as your favorite heroes from the original — Tallahassee, Columbus, Wichita and Little Rock — as well as new unlockable characters from the upcoming sequel.  The game embraces the game-like elements seen in the film by  incorporating everything from the “Rules” to “Zombie Kill of the Week”.  Use your special abilities, an arsenal of weapons and the essential Zombieland rules for survival to stay alive against huge numbers of uniquely grotesque and dangerous undead monstrosities in Zombieland: Double Tap- Road Trip’s story-based campaign mode, wave-based horde mode, and boss battles.",
-        _createdOn: 1617194295474,
+        name: "Roast Trout",
+        img: "assets/fish.jpg",
+        ingredients: [
+          "4 cups Ingredient 1",
+          "1 tbsp Ingredient 2",
+          "1 tbsp Ingredient 3",
+          "750 g  Ingredient 4",
+          "25 g Ingredient 5",
+        ],
+        steps: ["Prepare ingredients", "Mix ingredients", "Cook until done"],
+        _createdOn: 1613551388703,
       },
     },
-    comments: {},
+    comments: [
+      {
+        _ownerId: "6f080556-76d2-41e4-b7ef-5180f178f33b",
+        bookId: "126777f5-3277-42ad-b874-76d043b069cb",
+        comment: "asdfasdfasd asdfasdfasdf",
+        _createdOn: 1689825890341,
+        _id: "e19688e7-0e88-430c-9b31-57b68401b208",
+      },
+      {
+        _ownerId: "6f080556-76d2-41e4-b7ef-5180f178f33b",
+        bookId: "126777f5-3277-42ad-b874-76d043b069cb",
+        comment: "lka;lsdbf;aisjdbfl;asjdnf;asd",
+        _createdOn: 1689825928514,
+        _id: "04c4e5a1-ca00-411e-8425-3ddafd71e1b9",
+      },
+    ],
+    // comments: {
+    //   "0a272c58-b7ea-4e09-a000-7ec988248f66": {
+    //     _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    //     content: "Great recipe!",
+    //     recipeId: "8f414b4f-ab39-4d36-bedb-2ad69da9c830",
+    //     _createdOn: 1614260681375,
+    //     _id: "0a272c58-b7ea-4e09-a000-7ec988248f66",
+    //   },
+    // },
+    records: {
+      i01: {
+        name: "John1",
+        val: 1,
+        _createdOn: 1613551388703,
+      },
+      i02: {
+        name: "John2",
+        val: 1,
+        _createdOn: 1613551388713,
+      },
+      i03: {
+        name: "John3",
+        val: 2,
+        _createdOn: 1613551388723,
+      },
+      i04: {
+        name: "John4",
+        val: 2,
+        _createdOn: 1613551388733,
+      },
+      i05: {
+        name: "John5",
+        val: 2,
+        _createdOn: 1613551388743,
+      },
+      i06: {
+        name: "John6",
+        val: 3,
+        _createdOn: 1613551388753,
+      },
+      i07: {
+        name: "John7",
+        val: 3,
+        _createdOn: 1613551388763,
+      },
+      i08: {
+        name: "John8",
+        val: 2,
+        _createdOn: 1613551388773,
+      },
+      i09: {
+        name: "John9",
+        val: 3,
+        _createdOn: 1613551388783,
+      },
+      i10: {
+        name: "John10",
+        val: 1,
+        _createdOn: 1613551388793,
+      },
+    },
+    catches: {
+      "07f260f4-466c-4607-9a33-f7273b24f1b4": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        angler: "Paulo Admorim",
+        weight: 636,
+        species: "Atlantic Blue Marlin",
+        location: "Vitoria, Brazil",
+        bait: "trolled pink",
+        captureTime: 80,
+        _createdOn: 1614760714812,
+        _id: "07f260f4-466c-4607-9a33-f7273b24f1b4",
+      },
+      "bdabf5e9-23be-40a1-9f14-9117b6702a9d": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        angler: "John Does",
+        weight: 554,
+        species: "Atlantic Blue Marlin",
+        location: "Buenos Aires, Argentina",
+        bait: "trolled pink",
+        captureTime: 120,
+        _createdOn: 1614760782277,
+        _id: "bdabf5e9-23be-40a1-9f14-9117b6702a9d",
+      },
+    },
+    furniture: {},
+    orders: {},
+    movies: {
+      "1240549d-f0e0-497e-ab99-eb8f703713d7": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        title: "Black Widow",
+        description:
+          "Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Comes on the screens 2020.",
+        img: "https://miro.medium.com/max/735/1*akkAa2CcbKqHsvqVusF3-w.jpeg",
+        _createdOn: 1614935055353,
+        _id: "1240549d-f0e0-497e-ab99-eb8f703713d7",
+      },
+      "143e5265-333e-4150-80e4-16b61de31aa0": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        title: "Wonder Woman 1984",
+        description:
+          "Diana must contend with a work colleague and businessman, whose desire for extreme wealth sends the world down a path of destruction, after an ancient artifact that grants wishes goes missing.",
+        img: "https://pbs.twimg.com/media/ETINgKwWAAAyA4r.jpg",
+        _createdOn: 1614935181470,
+        _id: "143e5265-333e-4150-80e4-16b61de31aa0",
+      },
+      "a9bae6d8-793e-46c4-a9db-deb9e3484909": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        title: "Top Gun 2",
+        description:
+          "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
+        img: "https://i.pinimg.com/originals/f2/a4/58/f2a458048757bc6914d559c9e4dc962a.jpg",
+        _createdOn: 1614935268135,
+        _id: "a9bae6d8-793e-46c4-a9db-deb9e3484909",
+      },
+    },
+    likes: [
+      {
+        _ownerId: "6f080556-76d2-41e4-b7ef-5180f178f33b",
+        bookId: "126777f5-3277-42ad-b874-76d043b069cb",
+        _createdOn: 1689825890341,
+        _id: "0",
+      },
+      {
+        _ownerId: "6f080556-76d2-41e4-b7ef-5180f178f33b",
+        bookId: "126777f5-3277-42ad-b874-76d043b069cb",
+        _createdOn: 1689825928514,
+        _id: "1",
+      },
+    ],
+    ideas: {
+      "833e0e57-71dc-42c0-b387-0ce0caf5225e": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        title: "Best Pilates Workout To Do At Home",
+        description:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima possimus eveniet ullam aspernatur corporis tempore quia nesciunt nostrum mollitia consequatur. At ducimus amet aliquid magnam nulla sed totam blanditiis ullam atque facilis corrupti quidem nisi iusto saepe, consectetur culpa possimus quos? Repellendus, dicta pariatur! Delectus, placeat debitis error dignissimos nesciunt magni possimus quo nulla, fuga corporis maxime minus nihil doloremque aliquam quia recusandae harum. Molestias dolorum recusandae commodi velit cum sapiente placeat alias rerum illum repudiandae? Suscipit tempore dolore autem, neque debitis quisquam molestias officia hic nesciunt? Obcaecati optio fugit blanditiis, explicabo odio at dicta asperiores distinctio expedita dolor est aperiam earum! Molestias sequi aliquid molestiae, voluptatum doloremque saepe dignissimos quidem quas harum quo. Eum nemo voluptatem hic corrupti officiis eaque et temporibus error totam numquam sequi nostrum assumenda eius voluptatibus quia sed vel, rerum, excepturi maxime? Pariatur, provident hic? Soluta corrupti aspernatur exercitationem vitae accusantium ut ullam dolor quod!",
+        img: "./images/best-pilates-youtube-workouts-2__medium_4x3.jpg",
+        _createdOn: 1615033373504,
+        _id: "833e0e57-71dc-42c0-b387-0ce0caf5225e",
+      },
+      "247efaa7-8a3e-48a7-813f-b5bfdad0f46c": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        title: "4 Eady DIY Idea To Try!",
+        description:
+          "Similique rem culpa nemo hic recusandae perspiciatis quidem, quia expedita, sapiente est itaque optio enim placeat voluptates sit, fugit dignissimos tenetur temporibus exercitationem in quis magni sunt vel. Corporis officiis ut sapiente exercitationem consectetur debitis suscipit laborum quo enim iusto, labore, quod quam libero aliquid accusantium! Voluptatum quos porro fugit soluta tempore praesentium ratione dolorum impedit sunt dolores quod labore laudantium beatae architecto perspiciatis natus cupiditate, iure quia aliquid, iusto modi esse!",
+        img: "./images/brightideacropped.jpg",
+        _createdOn: 1615033452480,
+        _id: "247efaa7-8a3e-48a7-813f-b5bfdad0f46c",
+      },
+      "b8608c22-dd57-4b24-948e-b358f536b958": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        title: "Dinner Recipe",
+        description:
+          "Consectetur labore et corporis nihil, officiis tempora, hic ex commodi sit aspernatur ad minima? Voluptas nesciunt, blanditiis ex nulla incidunt facere tempora laborum ut aliquid beatae obcaecati quidem reprehenderit consequatur quis iure natus quia totam vel. Amet explicabo quidem repellat unde tempore et totam minima mollitia, adipisci vel autem, enim voluptatem quasi exercitationem dolor cum repudiandae dolores nostrum sit ullam atque dicta, tempora iusto eaque! Rerum debitis voluptate impedit corrupti quibusdam consequatur minima, earum asperiores soluta. A provident reiciendis voluptates et numquam totam eveniet! Dolorum corporis libero dicta laborum illum accusamus ullam?",
+        img: "./images/dinner.jpg",
+        _createdOn: 1615033491967,
+        _id: "b8608c22-dd57-4b24-948e-b358f536b958",
+      },
+    },
+    catalog: {
+      "53d4dbf5-7f41-47ba-b485-43eccb91cb95": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        make: "Table",
+        model: "Swedish",
+        year: 2015,
+        description: "Medium table",
+        price: 235,
+        img: "./images/table.png",
+        material: "Hardwood",
+        _createdOn: 1615545143015,
+        _id: "53d4dbf5-7f41-47ba-b485-43eccb91cb95",
+      },
+      "f5929b5c-bca4-4026-8e6e-c09e73908f77": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        make: "Sofa",
+        model: "ES-549-M",
+        year: 2018,
+        description: "Three-person sofa, blue",
+        price: 1200,
+        img: "./images/sofa.jpg",
+        material: "Frame - steel, plastic; Upholstery - fabric",
+        _createdOn: 1615545572296,
+        _id: "f5929b5c-bca4-4026-8e6e-c09e73908f77",
+      },
+      "c7f51805-242b-45ed-ae3e-80b68605141b": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        make: "Chair",
+        model: "Bright Dining Collection",
+        year: 2017,
+        description: "Dining chair",
+        price: 180,
+        img: "./images/chair.jpg",
+        material: "Wood laminate; leather",
+        _createdOn: 1615546332126,
+        _id: "c7f51805-242b-45ed-ae3e-80b68605141b",
+      },
+    },
+    teams: {
+      "34a1cab1-81f1-47e5-aec3-ab6c9810efe1": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        name: "Storm Troopers",
+        logoUrl: "/assets/atat.png",
+        description: "These ARE the droids we're looking for",
+        _createdOn: 1615737591748,
+        _id: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
+      },
+      "dc888b1a-400f-47f3-9619-07607966feb8": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        name: "Team Rocket",
+        logoUrl: "/assets/rocket.png",
+        description: "Gotta catch 'em all!",
+        _createdOn: 1615737655083,
+        _id: "dc888b1a-400f-47f3-9619-07607966feb8",
+      },
+      "733fa9a1-26b6-490d-b299-21f120b2f53a": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        name: "Minions",
+        logoUrl: "/assets/hydrant.png",
+        description:
+          "Friendly neighbourhood jelly beans, helping evil-doers succeed.",
+        _createdOn: 1615737688036,
+        _id: "733fa9a1-26b6-490d-b299-21f120b2f53a",
+      },
+    },
+    members: {
+      "cc9b0a0f-655d-45d7-9857-0a61c6bb2c4d": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        teamId: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
+        status: "member",
+        _createdOn: 1616236790262,
+        _updatedOn: 1616236792930,
+      },
+      "61a19986-3b86-4347-8ca4-8c074ed87591": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
+        status: "member",
+        _createdOn: 1616237188183,
+        _updatedOn: 1616237189016,
+      },
+      "8a03aa56-7a82-4a6b-9821-91349fbc552f": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        teamId: "733fa9a1-26b6-490d-b299-21f120b2f53a",
+        status: "member",
+        _createdOn: 1616237193355,
+        _updatedOn: 1616237195145,
+      },
+      "9be3ac7d-2c6e-4d74-b187-04105ab7e3d6": {
+        _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
+        status: "member",
+        _createdOn: 1616237231299,
+        _updatedOn: 1616237235713,
+      },
+      "280b4a1a-d0f3-4639-aa54-6d9158365152": {
+        _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+        teamId: "dc888b1a-400f-47f3-9619-07607966feb8",
+        status: "member",
+        _createdOn: 1616237257265,
+        _updatedOn: 1616237278248,
+      },
+      "e797fa57-bf0a-4749-8028-72dba715e5f8": {
+        _ownerId: "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+        teamId: "34a1cab1-81f1-47e5-aec3-ab6c9810efe1",
+        status: "member",
+        _createdOn: 1616237272948,
+        _updatedOn: 1616237293676,
+      },
+    },
+    books: {
+      "833e0e57-71dc-42c0-b387-0ce0caf5225e": {
+        _ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+        title: "За кожата на една врата",
+        description:
+          "'За кожата на една врата' е книга с разкази, човешки, приключенски, написани  с богат и буден език, пропити с хумор , с енергията на младостта и нейните пътувания навътре в себе си, далеч по света и по познатите ни улици, с усмивка, с болка, искрени, мъдри  и изключително приятни за четене.",
+        imgUrl:
+          "https://www.ciela.com/media/catalog/product/cache/9a7ceae8a5abbd0253425b80f9ef99a5/_/-/_-_-_-_-__5_12.jpg",
+        _createdOn: 1615033373504,
+        _id: "833e0e57-71dc-42c0-b387-0ce0caf5225e",
+        author: "Николай Хашъмов - Хъшо",
+        genre: "разкази",
+        year: 2017,
+        _createdOn: 1616237272948,
+        _updatedOn: 1616237293676,
+      },
+    },
   };
   var rules$1 = {
     users: {
@@ -1480,6 +1795,19 @@
       ".read": ["Owner"],
       ".update": false,
       ".delete": false,
+    },
+    members: {
+      ".update": "isOwner(user, get('teams', data.teamId))",
+      ".delete":
+        "isOwner(user, get('teams', data.teamId)) || isOwner(user, data)",
+      "*": {
+        teamId: {
+          ".update": "newData.teamId = data.teamId",
+        },
+        status: {
+          ".create": "newData.status = 'pending'",
+        },
+      },
     },
   };
   var settings = {
